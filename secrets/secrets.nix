76c97558e41db04ed72ccc943c2880e0177dce22
ErrorNoInternet <errornointernet@envs.net>
2023-09-15 20:05:37 +0800

    Change host keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
     NixBtw = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzdpxex2GlFVf5G2qsh3Ixa/XCMjnbq4JSTmAev7WYJ";
-    Crix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIt0sioZlJqPem8SZfy/zoJGprGLYBFshM71WwQfa4bG";
+    Crix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8EVE+n6AqS8NWfZRjqLSI3HY+2YEWEsDo9oPSMt5C+";
     systems = [ NixBtw Crix ];
 in
 {
