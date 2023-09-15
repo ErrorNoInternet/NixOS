5034b2833707b30c7b895fc5792b7b731c38349a
ErrorNoInternet <errornointernet@envs.net>
2023-09-16 00:41:55 +0800

    Make home-manager nvim module

--- home-manager/hosts/ErrorNoPhone.nix
+++ home-manager/hosts/ErrorNoPhone.nix
@@ -4,7 +4,7 @@
     imports = [
         ../modules/bat.nix
         ../modules/nix-on-droid/zsh.nix
-        ../modules/nvim.nix
+        ../modules/nix-on-droid/nvim.nix
         ../modules/tmux.nix
         ../modules/zoxide.nix
     ];
