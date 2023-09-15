e1cc090e2d8a6152ef3b8dc5beec166657fd9889
ErrorNoInternet <errornointernet@envs.net>
2023-09-15 17:52:48 +0800

    Get path of .age file

--- server/base.nix
+++ server/base.nix
@@ -32,7 +32,7 @@
     networking = {
         wireless = {
             enable = true;
-            environmentFile = config.age.secrets.wireless-networks;
+            environmentFile = config.age.secrets.wireless-networks.path;
             networks = {
                 "@ssid1@".psk = "@psk1@";
                 "@ssid2@".psk = "@psk2@";
