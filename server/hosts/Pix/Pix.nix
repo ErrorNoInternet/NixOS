6b3e2a38d42c9835cbbbe50e7c4026e359f2fb05
ErrorNoInternet <errornointernet@envs.net>
2023-09-22 17:02:59 +0800

    Add drives

--- server/hosts/Pix/Pix.nix
+++ server/hosts/Pix/Pix.nix
@@ -3,4 +3,17 @@
 {
     networking.hostName = "Pix";
     environment.variables.HOSTNAME = "Pix";
+
+    filesystems = {
+        "/mnt/drive1" = {
+            device = "/dev/disk/by-uuid/fc102db2-60b8-43e1-8b21-40a589edfdda";
+            fsType = "btrfs";
+            options = [ "x-systemd.automount" "noauto" ];
+        };
+        "/mnt/drive3" = {
+            device = "/dev/disk/by-uuid/6a03c0f9-5c76-4a08-9091-aba7239a6429";
+            fsType = "btrfs";
+            options = [ "x-systemd.automount" "noauto" ];
+        };
+    };
 }
