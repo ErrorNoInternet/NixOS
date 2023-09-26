960b3cfd89b1b55e56c6546726d46d04053d36f6
ErrorNoInternet <errornointernet@envs.net>
2023-09-26 14:06:09 +0800

    Add update-ddns.sh secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,11 @@
 let
-    NixBtw = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzdpxex2GlFVf5G2qsh3Ixa/XCMjnbq4JSTmAev7WYJ";
-    Crix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8EVE+n6AqS8NWfZRjqLSI3HY+2YEWEsDo9oPSMt5C+";
-    systems = [ NixBtw Crix ];
+    NixBtw-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDzdpxex2GlFVf5G2qsh3Ixa/XCMjnbq4JSTmAev7WYJ";
+    Crix-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8EVE+n6AqS8NWfZRjqLSI3HY+2YEWEsDo9oPSMt5C+";
+    Pix-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHoKZCpEfJ+rBCndkSlH3qEMypvSuJRdieSwPVMCKZB";
+    systems = [ NixBtw-user Crix-system Pix-system ];
 in
 {
-    "nheko-access-token.age".publicKeys = [ NixBtw ];
+    "nheko-access-token.age".publicKeys = [ NixBtw-user ];
+    "update-ddns.sh.age".publicKeys = [ Pix-system ];
     "wireless-networks.age".publicKeys = systems;
 }
