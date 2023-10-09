95cf7d032b0e88b927dd6b9f83b2b14f5d18f668
ErrorNoInternet <errornointernet@envs.net>
2023-10-09 20:29:05 +0800

    Add more wallpapers

--- home-manager/modules/hyprpaper.nix
+++ home-manager/modules/hyprpaper.nix
@@ -2,9 +2,12 @@
     home.file = {
         ".config/hypr/hyprpaper.conf" = {
             text = ''
-preload = ~/pictures/wallpapers/wallpaper0.png
-wallpaper = ,~/pictures/wallpapers/wallpaper0.png
-'';
+                preload = ~/pictures/wallpapers/wallpaper0.png
+                preload = ~/pictures/wallpapers/wallpaper1.png
+                preload = ~/pictures/wallpapers/wallpaper2.png
+                preload = ~/pictures/wallpapers/wallpaper3.png
+                wallpaper = ,~/pictures/wallpapers/wallpaper1.png
+            '';
         };
     };
 }
