c53d5a0e613510cad524bb690d0cce79257115f4
ErrorNoInternet <errornointernet@envs.net>
2023-10-12 17:43:28 +0800

    Fix JetBrains icon

--- home-manager/modules/hyprland-autoname-workspaces.nix
+++ home-manager/modules/hyprland-autoname-workspaces.nix
@@ -17,7 +17,7 @@
       "discord" = "󰙯 "
       "firefox" = " "
       "Gimp-2.10" = " "
-      "jetbrains-idea-ce" = " "
+      "jetbrains-idea-ce" = ""
       "kitty" = ""
       "Minecraft.*" = "󰍳 "
       "mpv" = " "
