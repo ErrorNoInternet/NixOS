91e0d831ec86d464437f33580b15a710aaae631b
ErrorNoInternet <errornointernet@envs.net>
2023-10-12 17:49:49 +0800

    Update self-hosted binary cache

--- workstation/modules/selfhosted-cache.nix
+++ workstation/modules/selfhosted-cache.nix
@@ -1,6 +1,6 @@
 {
   nix.settings = {
-    substituters = [ "http://192.168.0.100:8082" ];
-    trusted-public-keys = [ "spongix:ZfSaeOzzyINMxU4w4r+8R9g5stHvVG6xvOjUL8J6gN0=" ];
+    substituters = [ "http://192.168.0.100:8082/ErrorNoBinaries" ];
+    trusted-public-keys = [ "ErrorNoBinaries:im2fJ1t41XAwp2S+DMgSI0VFKxS+jpz/XIOs/s9iLFg=" ];
   };
 }
