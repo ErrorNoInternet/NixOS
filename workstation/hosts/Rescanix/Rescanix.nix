a262f9315d4de05ef8d9837e314237880cf69892
ErrorNoInternet <errornointernet@envs.net>
2023-10-22 22:19:10 +0800

    Force boot timeout

--- workstation/hosts/Rescanix/Rescanix.nix
+++ workstation/hosts/Rescanix/Rescanix.nix
@@ -1,10 +1,13 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   networking.hostName = "Rescanix";
   environment.variables.HOSTNAME = "Rescanix";
 
-  boot.loader.systemd-boot.memtest86.enable = true;
+  boot.loader = {
+    timeout = lib.mkForce 5;
+    systemd-boot.memtest86.enable = true;
+  };
   services.xserver.desktopManager.plasma5.enable = true;
   environment.systemPackages = with pkgs; [];
 }
