67d9c54a4ad16a9bba739172fefca2bdd2c927ee
ErrorNoInternet <errornointernet@envs.net>
2023-10-23 16:31:18 +0800

    Test specialization for Rescanix

--- workstation/modules/nvidia-prime.nix
+++ workstation/modules/nvidia-prime.nix
@@ -1,13 +1,17 @@
 {
-  hardware.nvidia = {
-    prime = {
-      offload = {
-        enable = true;
-        enableOffloadCmd = true;
-      };
+  specialization = {
+    nvidia-prime = {
+      hardware.nvidia = {
+        prime = {
+          offload = {
+            enable = true;
+            enableOffloadCmd = true;
+          };
 
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
+          intelBusId = "PCI:0:2:0";
+          nvidiaBusId = "PCI:1:0:0";
+        };
+      };
     };
   };
 }
--- workstation/modules/nvidia.nix
+++ workstation/modules/nvidia.nix
@@ -1,11 +1,15 @@
 {
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    modesetting.enable = true;
+  specialization = {
+    nvidia = {
+      hardware.opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
+      };
+      services.xserver.videoDrivers = [ "nvidia" ];
+      hardware.nvidia = {
+        modesetting.enable = true;
+      };
+    };
   };
 }
