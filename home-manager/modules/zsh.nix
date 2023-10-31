3509ce6e0ea7ed8361081cfa66b0a12fc3adbf3c
ErrorNoInternet <errornointernet@envs.net>
2023-10-31 19:24:44 +0800

    Add alias for mounting LUKS

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -30,9 +30,10 @@
       la = "ls -a";
       lla = "ls -al";
       grep = "grep --color";
-
-      switch = "sudo nixos-rebuild switch";
-      home-switch = "home-manager switch";
+  
+      md = "sudo cryptsetup luksOpen /dev/disk/by-uuid/9cbb87fd-6e5a-45b3-88ee-22d369738be5 luks-btank; sudo mount /dev/mapper/luks-btank /mnt/data";
+      switch = "sudo nixos-rebuild switch --flake .";
+      home-switch = "home-manager switch --flake .";
       f = "fastfetch";
       cm = "cmatrix -C blue";
       py = "python3";
