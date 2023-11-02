2ab4f94b24c5d1ec409ee9d890ab75f3e13ca1e3
ErrorNoInternet <errornointernet@envs.net>
2023-11-03 02:34:03 +0800

    Minor fastfetch changes

--- home-manager/modules/fastfetch.nix
+++ home-manager/modules/fastfetch.nix
@@ -2,9 +2,10 @@
   home.file = {
     ".config/fastfetch/minimal.conf" = {
       text = ''
---os-key " os"
+--os-key " system"
 --os-format "{2} {9} ({12})"
 --kernel-key " kernel"
+--kernel-format "{1} {2}"
 --uptime-key " uptime"
 --packages-key "󰆧 packages"
 --packages-format "{9}+{10} (nix)"
@@ -12,17 +13,18 @@
 --disk-format "{1} / {2} ({3})"
 --terminal-key " terminal"
 --disk-folders /
+--logo-padding-right 3
+--key-width 13
+--separator ""
 
 --set-keyless colors=\e[37m  \e[36m  \e[35m  \e[34m  \e[33m  \e[32m  \e[31m  \e[30m
 --logo nixos_small
---separator "  "
 --structure os:kernel:uptime:packages:disk:terminal:colors
 '';
     };
     ".config/fastfetch/config.conf" = {
       text = ''
 --disk-folders /
---logo-padding-right 2
 
 --structure title:separator:os:kernel:uptime:packages:shell:display:wm:theme:icons:cursor:terminalfont:terminal:processes:memory:disk:break:colors
 '';
