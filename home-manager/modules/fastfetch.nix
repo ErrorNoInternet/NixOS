525beffa371bf54b63f97445e5876145fb0ca44a
ErrorNoInternet <errornointernet@envs.net>
2023-11-07 17:29:59 +0800

    Format fastfetch.nix

--- home-manager/modules/fastfetch.nix
+++ home-manager/modules/fastfetch.nix
@@ -1,33 +1,29 @@
 {
   home.file = {
-    ".config/fastfetch/minimal.conf" = {
-      text = ''
---os-key " system  "
---os-format "{2} {9} ({12})"
---kernel-key " kernel  "
---kernel-format "{1} {2}"
---uptime-key " uptime  "
---packages-key "󰆧 packages"
---packages-format "{9}+{10} (nix)"
---memory-key "󰍛 memory  "
---disk-key "󱥎 storage "
---disk-format "{1} / {2} ({3})"
---disk-folders /
---logo-padding-right 3
---key-width 14
---set-keyless colors=\e[37m  \e[36m  \e[35m  \e[34m  \e[33m  \e[32m  \e[31m  \e[30m
---separator " "
---logo nixos_small
+    ".config/fastfetch/minimal.conf".text = ''
+        --os-key " system  "
+        --os-format "{2} {9} ({12})"
+        --kernel-key " kernel  "
+        --kernel-format "{1} {2}"
+        --uptime-key " uptime  "
+        --packages-key "󰆧 packages"
+        --packages-format "{9}+{10} (nix)"
+        --memory-key "󰍛 memory  "
+        --disk-key "󱥎 storage "
+        --disk-format "{1} / {2} ({3})"
+        --disk-folders /
+        --logo-padding-right 3
+        --key-width 14
+        --set-keyless colors=\e[37m  \e[36m  \e[35m  \e[34m  \e[33m  \e[32m  \e[31m  \e[30m
+        --separator " "
+        --logo nixos_small
 
---structure os:kernel:uptime:packages:memory:disk:colors
-'';
-    };
-    ".config/fastfetch/config.conf" = {
-      text = ''
---disk-folders /
+        --structure os:kernel:uptime:packages:memory:disk:colors
+    '';
+    ".config/fastfetch/config.conf".text = ''
+        --disk-folders /
 
---structure title:separator:os:kernel:uptime:packages:shell:display:wm:theme:icons:cursor:terminalfont:terminal:processes:memory:disk:break:colors
-'';
-    };
+        --structure title:separator:os:kernel:uptime:packages:shell:display:wm:theme:icons:cursor:terminalfont:terminal:processes:memory:disk:break:colors
+    '';
   };
 }
