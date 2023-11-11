1eb96f3728206c01377c0778ebbd81a6121c5bea
ErrorNoInternet <errornointernet@envs.net>
2023-11-11 22:22:58 +0800

    feat: faster tmux resizing

--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -25,19 +25,23 @@
 
       set -g pane-active-border-style 'fg=colour4'
 
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind '%' split-window -h -c "#{pane_current_path}"
       bind -n M-Left previous-window
       bind -n M-Right next-window
       bind -n S-Left previous-window
       bind -n S-Right next-window
-      bind '"' split-window -v -c "#{pane_current_path}"
-      bind '%' split-window -h -c "#{pane_current_path}"
-      bind -T copy-mode-vi y send -X copy-pipe-and-cancel 'wl-copy'
-      bind -T copy-mode-vi v send -X begin-selection
-      bind -T copy-mode-vi C-v send -X rectangle-toggle
+      bind -r -T prefix C-Down resize-pane -D 3
+      bind -r -T prefix C-Left resize-pane -L 5
+      bind -r -T prefix C-Right resize-pane -R 5
+      bind -r -T prefix C-Up resize-pane -U 3
+      bind -T copy-mode-vi 'End' send -X end-of-line
+      bind -T copy-mode-vi 'Home' send -X start-of-line
       bind -T copy-mode-vi C-Left send -X previous-word
       bind -T copy-mode-vi C-Right send -X next-word
-      bind -T copy-mode-vi 'Home' send -X start-of-line
-      bind -T copy-mode-vi 'End'  send -X end-of-line
+      bind -T copy-mode-vi C-v send -X rectangle-toggle
+      bind -T copy-mode-vi v send -X begin-selection
+      bind -T copy-mode-vi y send -X copy-pipe-and-cancel 'wl-copy'
     '';
   };
 }
