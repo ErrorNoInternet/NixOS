c567549ceaa3ea25ec87d4d874a0799f6cd2931d
ErrorNoInternet <errornointernet@envs.net>
2023-11-28 21:39:00 +0800

    refactor: make workstation profiles

--- workstation/default.nix
+++ workstation/default.nix
@@ -6,10 +6,10 @@
 }: let
   defaultModule = {
     imports = [
-      ./common.nix
       ../shared
-      ./locations/china.nix
       ../shared/caches/ErrorNoBinaries.nix
+      ./common.nix
+      ./locations/china.nix
     ];
   };
   inherit (inputs.nixpkgs.lib) nixosSystem;
@@ -23,23 +23,23 @@ in {
       specialArgs = {inherit inputs self;};
       modules = [
         defaultModule
-        ./hosts/NixBtw/hardware-configuration.nix
-        ./hosts/NixBtw/NixBtw.nix
-        ./modules/android-development.nix
-        ./modules/bluetooth.nix
-        ./modules/gaming.nix
-        ./modules/nvidia-options.nix
-        ./modules/nvidia.nix
-        ./modules/video-acceleration.nix
-        ./modules/virtualization.nix
-        ./programs/clamav.nix
-        ./programs/fish.nix
-        ./programs/hyprland.nix
-        ./programs/openrgb.nix
         ../shared/caches/hyprland.nix
         ../shared/caches/nix-gaming.nix
         ../shared/modules/aarch64-emulation.nix
         ../shared/modules/btrfs-subvolume-compression.nix
+        ./hosts/NixBtw/hardware-configuration.nix
+        ./hosts/NixBtw/NixBtw.nix
+        ./modules/bluetooth.nix
+        ./modules/nvidia-options.nix
+        ./modules/nvidia.nix
+        ./modules/video-acceleration.nix
+        ./modules/virtualization.nix
+        ./profiles/android-development.nix
+        ./profiles/gaming.nix
+        ./programs/clamav.nix
+        ./programs/fish.nix
+        ./programs/hyprland.nix
+        ./programs/openrgb.nix
       ];
     };
     Rescanix = nixosSystem {
@@ -50,24 +50,24 @@ in {
       specialArgs = {inherit inputs self;};
       modules = [
         defaultModule
-        ./hosts/Rescanix/hardware-configuration.nix
-        ./hosts/Rescanix/Rescanix.nix
-        ./modules/bluetooth.nix
-        ./modules/gaming.nix
-        ./modules/nvidia-options.nix
-        ./modules/nvidia.nix
-        ./modules/video-acceleration.nix
-        ./modules/virtualization.nix
-        ./modules/vm-guest.nix
-        ./modules/zfs.nix
-        ./programs/clamav.nix
-        ./programs/fish.nix
-        ./programs/hyprland.nix
-        ./programs/openrgb.nix
         ../shared/caches/hyprland.nix
         ../shared/caches/nix-gaming.nix
         ../shared/modules/aarch64-emulation.nix
         ../shared/modules/btrfs-compression.nix
+        ./hosts/Rescanix/hardware-configuration.nix
+        ./hosts/Rescanix/Rescanix.nix
+        ./modules/bluetooth.nix
+        ./modules/nvidia-options.nix
+        ./modules/nvidia.nix
+        ./modules/video-acceleration.nix
+        ./modules/virtualization.nix
+        ./modules/zfs.nix
+        ./profiles/gaming.nix
+        ./profiles/vm-guest.nix
+        ./programs/clamav.nix
+        ./programs/fish.nix
+        ./programs/hyprland.nix
+        ./programs/openrgb.nix
       ];
     };
   };
