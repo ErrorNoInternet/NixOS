306e6e04cdbf88bf644003c3985d394c1484ec82
ErrorNoInternet <errornointernet@envs.net>
2023-11-29 10:51:36 +0800

    feat: swww -> hyprpaper

--- home-manager/profiles/wm/default.nix
+++ home-manager/profiles/wm/default.nix
@@ -10,23 +10,23 @@
     ../../modules/cursor.nix
     ../../programs/dunst.nix
     ../../programs/rofi.nix
+    ../../wayland/hyprpaper.nix
     ../../wayland/waybar.nix
   ];
 
   home.packages = with pkgs; [
+    (self.packages.${pkgs.system}.swaylock.override {
+      colorScheme = config.colorScheme;
+      font = custom.font;
+    })
     cliphist
     inputs.hyprwm-contrib.packages.${pkgs.system}.grimblast
     pavucontrol
     ripdrag
     self.packages.${pkgs.system}.brightness
     self.packages.${pkgs.system}.pavolume
-    (self.packages.${pkgs.system}.swaylock.override {
-      colorScheme = config.colorScheme;
-      font = custom.font;
-    })
     slurp
     swayidle
-    swww
     wf-recorder
     wl-clipboard
   ];
--- home-manager/wayland/hyprland.nix
+++ home-manager/wayland/hyprland.nix
@@ -28,7 +28,7 @@
         ",preferred,auto,auto"
       ];
       exec-once = [
-        "sleep 1; swww init"
+        "hyprpaper"
         "hyprctl setcursor ${custom.pointerCursor.name} ${builtins.toString custom.pointerCursor.size}"
         "swayidle -w timeout 600 'custom-swaylock 10 10' before-sleep 'custom-swaylock 0 1'"
         "hyprland-autoname-workspaces"
