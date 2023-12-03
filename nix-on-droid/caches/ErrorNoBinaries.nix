92c172eff148d2826d6b3a0c0aea74d12178de80
ErrorNoInternet <errornointernet@envs.net>
2023-12-03 13:36:34 +0800

    fix(nix-on-droid): ErrorNoBinaries cache

--- nix-on-droid/caches/ErrorNoBinaries.nix
+++ nix-on-droid/caches/ErrorNoBinaries.nix
@@ -6,7 +6,7 @@
     ];
     trustedPublicKeys = [
       "ErrorNoBinaries:im2fJ1t41XAwp2S+DMgSI0VFKxS+jpz/XIOs/s9iLFg="
-      "errornobinaries.cachix.org-1:H2M3PQPDikFPKfMlsfzKKfT5oZchHwPfYMhynYpJctw="
+      "errornobinaries.cachix.org-1:84oagGNCIsXxBTYmfTiP+lvWje7lIS294iqAtCpFsbU="
     ];
   };
 }
