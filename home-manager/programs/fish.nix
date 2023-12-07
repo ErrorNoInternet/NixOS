7fc56a792e90e82120da327a105e09c40afc0943
ErrorNoInternet <errornointernet@envs.net>
2023-12-08 01:19:34 +0800

    tweak: git blame alias

--- home-manager/programs/fish.nix
+++ home-manager/programs/fish.nix
@@ -53,7 +53,7 @@
       gl = "git log";
       gs = "git show";
       gr = "git restore";
-      gb = "git blame";
+      gbl = "git blame";
       ns = "nix shell";
       nr = "nix run";
       nd = "nix develop -c fish";
