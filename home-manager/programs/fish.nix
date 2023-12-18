fd61fabf3e2c4389e6f1a0a7c91b5ef2f6a0fa88
ErrorNoInternet <errornointernet@envs.net>
2023-12-18 11:12:23 +0800

    tweak(fish): rebuild alias

--- home-manager/programs/fish.nix
+++ home-manager/programs/fish.nix
@@ -27,8 +27,8 @@
       q = "exit";
 
       md = "sudo cryptsetup luksOpen /dev/disk/by-uuid/9cbb87fd-6e5a-45b3-88ee-22d369738be5 luks-btank && sudo mount /dev/mapper/luks-btank /mnt/data";
-      nixos-switch = "sudo nixos-rebuild switch --flake .";
-      home-switch = "home-manager switch --flake .";
+      nrs = "sudo nixos-rebuild switch --flake .";
+      hs = "home-manager switch --flake .";
       f = "${pkgs.fastfetch}/bin/fastfetch";
       mf = "${pkgs.fastfetch}/bin/fastfetch -c ~/.config/fastfetch/minimal.jsonc";
       mfa = "${pkgs.fastfetch}/bin/fastfetch -c ~/.config/fastfetch/nix-on-droid_minimal.jsonc";
