7fc94a41755ff253159ce6c77e1090290b1db032
ErrorNoInternet <errornointernet@envs.net>
2023-12-27 19:39:43 +0800

    refactor: remove dead code

--- home-manager/locations/china.nix
+++ home-manager/locations/china.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{
   nix.settings = {
     substituters = ["https://mirror.sjtu.edu.cn/nix-channels/store"];
     trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
