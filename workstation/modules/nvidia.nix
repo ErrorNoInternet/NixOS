6141879cc28471ec8429b6f4c78ec496eea7b2ba
ErrorNoInternet <errornointernet@envs.net>
2023-12-28 14:42:42 +0800

    fix(nvidia): downgrade to 535

--- workstation/modules/nvidia.nix
+++ workstation/modules/nvidia.nix
@@ -4,22 +4,25 @@
   ...
 }: {
   config = lib.mkIf (config.specialisation != {}) {
-    hardware.opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-    };
     services.xserver.videoDrivers = ["nvidia"];
-    hardware.nvidia = {
-      modesetting.enable = true;
-      prime = {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
+    hardware = {
+      opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
+      };
+      nvidia = {
+        package = config.boot.kernelPackages.nvidiaPackages.production;
+        modesetting.enable = true;
+        prime = {
+          offload = {
+            enable = true;
+            enableOffloadCmd = true;
+          };
 
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
+          intelBusId = "PCI:0:2:0";
+          nvidiaBusId = "PCI:1:0:0";
+        };
       };
     };
   };
