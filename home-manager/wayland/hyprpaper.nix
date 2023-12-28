3f3ccaf22c735c45e9677143f0b958f9e90f5be2
ErrorNoInternet <errornointernet@envs.net>
2023-12-28 16:05:25 +0800

    feat(hyprpaper): turn IPC back on

--- home-manager/wayland/hyprpaper.nix
+++ home-manager/wayland/hyprpaper.nix
@@ -4,10 +4,11 @@
   pkgs,
   ...
 }: {
-  home.packages = [pkgs.hyprpaper];
-  home.file.".config/hypr/hyprpaper.conf".text = ''
-    ipc = off
-    preload = ~/pictures/wallpapers/${lib.strings.toLower custom.predefinedColorScheme}/wallpaper1.png
-    wallpaper = ,~/pictures/wallpapers/${lib.strings.toLower custom.predefinedColorScheme}/wallpaper1.png
-  '';
+  home = {
+    packages = [pkgs.hyprpaper];
+    file.".config/hypr/hyprpaper.conf".text = ''
+      preload = ~/pictures/wallpapers/${lib.strings.toLower custom.predefinedColorScheme}/wallpaper1.png
+      wallpaper = ,~/pictures/wallpapers/${lib.strings.toLower custom.predefinedColorScheme}/wallpaper1.png
+    '';
+  };
 }
