31e3a702b73ebcd564b5ebf175fddc2bd9a159f3
ErrorNoInternet <errornointernet@envs.net>
2023-12-31 14:47:08 +0800

    refactor(server): attic as NixOS module

--- server/modules/attic-cache.nix
+++ server/modules/attic-cache.nix
@@ -1,40 +1,34 @@
 {
+  config,
   inputs,
-  pkgs,
   ...
 }: let
   port = 7455;
+  storage = "/mnt/drive1/Ryan/nix/attic";
 in {
+  imports = [inputs.attic.nixosModules.atticd];
+  age.secrets.attic-server-token.file = ../../secrets/attic-server-token.age;
+
   networking.firewall.allowedTCPPorts = [port];
-  environment.etc."attic-cache.toml".text = ''
-    listen = "[::]:${builtins.toString port}"
-    token-hs256-secret-base64 = "OGZqWFV1QnZQaHdCSm5RVGFtRlBCTWRSSHB0b0hkY3dTeG9YN0F0SFBpdzAwdklUMlpQcTRJSFFRMzROSEFaaUJSZXlHYTl2MjdLQzVIR2pRS0ZJSTlydWZlSE5kT1dsYzVFRUxXUnY3Zkc1VVVCWjlxSHVzOVZpM1IzbU16WHE="
-
-    [database]
-    url = "sqlite:///mnt/drive1/Ryan/nix/attic/server.db"
-
-    [storage]
-    type = "local"
-    path = "/mnt/drive1/Ryan/nix/attic/storage"
-
-    [chunking]
-    nar-size-threshold = 0
-
-    [compression]
-    type = "zstd"
-    level = 3
-
-    [garbage-collection]
-    interval = "7 days"
-    default-retention-period = "3 months"
-  '';
-  systemd.services.attic-cache = {
-    wantedBy = ["multi-user.target"];
-    description = "Attic - a Nix binary cache";
-    serviceConfig = {
-      Type = "simple";
-      Restart = "always";
-      ExecStart = ''${inputs.attic.packages.${pkgs.system}.attic}/bin/atticd -f /etc/attic-cache.toml'';
+  services.atticd = {
+    enable = true;
+    credentialsFile = config.age.secrets.attic-server-token.path;
+    settings = {
+      listen = "[::]:${builtins.toString port}";
+      database.url = "sqlite://${storage}/server.db";
+      storage = {
+        type = "local";
+        path = "${storage}/storage";
+      };
+      compression = {
+        type = "zstd";
+        level = 3;
+      };
+      chunking.nar-size-threshold = 0;
+      garbage-collection = {
+        interval = "7 days";
+        default-retention-period = "2 months";
+      };
     };
   };
 }
