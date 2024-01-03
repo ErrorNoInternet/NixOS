471e753fb9c90555c46ef54879b91dce929c3247
ErrorNoInternet <errornointernet@envs.net>
2024-01-03 13:34:51 +0800

    feat(Pix): snapshots

--- server/hosts/Pix/default.nix
+++ server/hosts/Pix/default.nix
@@ -57,12 +57,22 @@
     "/mnt/drive1" = {
       device = "/dev/disk/by-uuid/fc102db2-60b8-43e1-8b21-40a589edfdda";
       fsType = "btrfs";
-      options = ["x-systemd.automount" "noauto"];
+      options = ["subvol=root" "x-systemd.automount" "noauto"];
+    };
+    "/mnt/snapshots/drive1" = {
+      device = "/dev/disk/by-uuid/fc102db2-60b8-43e1-8b21-40a589edfdda";
+      fsType = "btrfs";
+      options = ["subvol=snapshots" "x-systemd.automount" "noauto"];
     };
     "/mnt/drive3" = {
       device = "/dev/disk/by-uuid/6a03c0f9-5c76-4a08-9091-aba7239a6429";
       fsType = "btrfs";
-      options = ["x-systemd.automount" "noauto"];
+      options = ["subvol=root" "x-systemd.automount" "noauto"];
+    };
+    "/mnt/snapshots/drive3" = {
+      device = "/dev/disk/by-uuid/6a03c0f9-5c76-4a08-9091-aba7239a6429";
+      fsType = "btrfs";
+      options = ["subvol=snapshots" "x-systemd.automount" "noauto"];
     };
   };
 
