a005d9f10d9d4fc69d0154eb04068a1f55b3ab66
ErrorNoInternet <errornointernet@envs.net>
2024-01-03 22:36:52 +0800

    feat(fish): alias for git revert
    And more modularity surrounding git abbreviations

---
+++ home-manager/terminal/fish/git-abbreviations.nix
@@ -0,0 +1,26 @@
+{
+  programs.fish.shellAbbrs = {
+    g = "git";
+    ga = "git add";
+    gap = "git add -p";
+    gb = "git branch";
+    gbl = "git blame";
+    gc = "git commit -m";
+    gca = "git commit --amend";
+    gcf = "git clean -f";
+    gcl = "git clone";
+    gco = "git checkout";
+    gd = "git diff";
+    gdc = "git diff --compact-summary";
+    gds = "git diff --staged";
+    gl = "git log";
+    gp = "git push";
+    gpl = "git pull";
+    gr = "git restore";
+    gre = "git reset";
+    grv = "git revert";
+    gs = "git show";
+    gst = "git status";
+    gsw = "git switch";
+  };
+}
