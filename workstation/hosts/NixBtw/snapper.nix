248d4a5af9ecdfac46811976dc43acad89a2fb28
ErrorNoInternet <errornointernet@envs.net>
2024-01-06 04:17:01 +0800

    feat(workstation/NixBtw): snapper quotas

--- workstation/hosts/NixBtw/snapper.nix
+++ workstation/hosts/NixBtw/snapper.nix
@@ -7,10 +7,11 @@
         SUBVOLUME = "/mnt/data/backups";
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
+        QGROUP = "1/0";
 
         TIMELINE_LIMIT_HOURLY = 24;
         TIMELINE_LIMIT_DAILY = 0;
-        TIMELINE_LIMIT_WEEKLY = 4;
+        TIMELINE_LIMIT_WEEKLY = "1-4";
         TIMELINE_LIMIT_MONTHLY = 0;
         TIMELINE_LIMIT_YEARLY = 0;
       };
@@ -18,9 +19,10 @@
         SUBVOLUME = "/mnt/data/downloads";
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
+        QGROUP = "1/0";
 
-        TIMELINE_LIMIT_HOURLY = 12;
-        TIMELINE_LIMIT_DAILY = 3;
+        TIMELINE_LIMIT_HOURLY = "1-12";
+        TIMELINE_LIMIT_DAILY = "1-3";
         TIMELINE_LIMIT_MONTHLY = 0;
         TIMELINE_LIMIT_YEARLY = 0;
       };
@@ -28,9 +30,10 @@
         SUBVOLUME = "/mnt/data/games";
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
+        QGROUP = "1/0";
 
         TIMELINE_LIMIT_HOURLY = 24;
-        TIMELINE_LIMIT_DAILY = 14;
+        TIMELINE_LIMIT_DAILY = "7-14";
         TIMELINE_LIMIT_MONTHLY = 1;
         TIMELINE_LIMIT_YEARLY = 0;
       };
@@ -38,9 +41,10 @@
         SUBVOLUME = "/mnt/data/hacking";
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
+        QGROUP = "1/0";
 
         TIMELINE_LIMIT_HOURLY = 12;
-        TIMELINE_LIMIT_DAILY = 3;
+        TIMELINE_LIMIT_DAILY = "1-3";
         TIMELINE_LIMIT_MONTHLY = 0;
         TIMELINE_LIMIT_YEARLY = 0;
       };
@@ -48,10 +52,11 @@
         SUBVOLUME = "/mnt/data/media";
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
+        QGROUP = "1/0";
 
         TIMELINE_LIMIT_HOURLY = 24;
-        TIMELINE_LIMIT_DAILY = 7;
-        TIMELINE_LIMIT_WEEKLY = 4;
+        TIMELINE_LIMIT_DAILY = "3-7";
+        TIMELINE_LIMIT_WEEKLY = "1-4";
         TIMELINE_LIMIT_MONTHLY = 0;
         TIMELINE_LIMIT_YEARLY = 0;
       };
@@ -59,9 +64,10 @@
         SUBVOLUME = "/mnt/data/other";
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
+        QGROUP = "1/0";
 
-        TIMELINE_LIMIT_HOURLY = 24;
-        TIMELINE_LIMIT_DAILY = 7;
+        TIMELINE_LIMIT_HOURLY = "12-24";
+        TIMELINE_LIMIT_DAILY = "3-7";
         TIMELINE_LIMIT_MONTHLY = 1;
         TIMELINE_LIMIT_YEARLY = 0;
       };
@@ -69,18 +75,20 @@
         SUBVOLUME = "/mnt/data/scripts";
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
+        QGROUP = "1/0";
 
         TIMELINE_LIMIT_HOURLY = 24;
-        TIMELINE_LIMIT_DAILY = 14;
-        TIMELINE_LIMIT_MONTHLY = 1;
+        TIMELINE_LIMIT_DAILY = "7-14";
+        TIMELINE_LIMIT_MONTHLY = "0-1";
         TIMELINE_LIMIT_YEARLY = 0;
       };
       temporary = {
         SUBVOLUME = "/mnt/data/temporary";
         TIMELINE_CREATE = true;
         TIMELINE_CLEANUP = true;
+        QGROUP = "1/0";
 
-        TIMELINE_LIMIT_HOURLY = 24;
+        TIMELINE_LIMIT_HOURLY = "1-12";
         TIMELINE_LIMIT_DAILY = 0;
         TIMELINE_LIMIT_MONTHLY = 0;
         TIMELINE_LIMIT_YEARLY = 0;
