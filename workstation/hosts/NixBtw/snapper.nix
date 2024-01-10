883c25ea05606b0c13276f38fa1f38ee2187f1f4
ErrorNoInternet <errornointernet@envs.net>
2024-01-11 01:22:35 +0800

    feat(snapper): snapshot /home

--- workstation/hosts/NixBtw/snapper.nix
+++ workstation/hosts/NixBtw/snapper.nix
@@ -13,6 +13,18 @@
   services.snapper = {
     snapshotInterval = "*:0/30";
     configs = {
+      home = {
+        SUBVOLUME = "/home/ryan";
+        TIMELINE_CREATE = true;
+        TIMELINE_CLEANUP = true;
+
+        TIMELINE_LIMIT_HOURLY = 24;
+        TIMELINE_LIMIT_DAILY = 7;
+        TIMELINE_LIMIT_WEEKLY = 0;
+        TIMELINE_LIMIT_MONTHLY = 0;
+        TIMELINE_LIMIT_YEARLY = 0;
+      };
+
       backups = {
         SUBVOLUME = "/mnt/data/backups";
         TIMELINE_CREATE = true;
