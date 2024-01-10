32e95798d6c729ae825f8d308c0b48886bb3f2f6
ErrorNoInternet <errornointernet@envs.net>
2024-01-11 01:41:14 +0800

    feat(hyprland): blueman as dialog

--- home/programs/wayland/hyprland/windowrules.nix
+++ home/programs/wayland/hyprland/windowrules.nix
@@ -1,5 +1,6 @@
 let
   dialogs = [
+    "class:(.blueman-manager-wrapped)"
     "class:(xdg-desktop-portal-gtk)"
     "title:(File Upload)"
     "title:(Open)"
@@ -8,18 +9,23 @@ let
 in {
   wayland.windowManager.hyprland.settings.windowrulev2 =
     [
-      "float           ,class:(branchdialog)"
-      "float           ,class:(confirm)"
-      "float           ,class:(dialog)"
-      "float           ,class:(error)"
-      "float           ,class:(mpv)"
-      "float           ,class:(notification)"
-      "float           ,class:(pavucontrol)"
-      "float           ,class:(vimiv)"
-      "size 50% 55%    ,class:(foot)"
+      "float,        class:(branchdialog)"
+      "float,        class:(confirm)"
+      "float,        class:(dialog)"
+      "float,        class:(error)"
+      "float,        class:(mpv)"
+      "float,        class:(notification)"
+      "float,        class:(pavucontrol)"
+      "float,        class:(vimiv)"
+      "size 50% 55%, class:(foot)"
     ]
     ++ (map (
-        dialog: "animation slide,${dialog}\ncenter,${dialog}\nfloat,${dialog}\nsize 60% 65%,${dialog}"
+        dialog: ''
+          animation slide, ${dialog}
+          center,          ${dialog}
+          float,           ${dialog}
+          size 60% 65%,    ${dialog}
+        ''
       )
       dialogs);
 }
