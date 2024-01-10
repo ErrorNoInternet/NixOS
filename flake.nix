8640a11502531b1630ee4dec3d0b84da3072675a
ErrorNoInternet <errornointernet@envs.net>
2024-01-10 23:16:44 +0800

    chore(flake): export homeManagerModules?

--- flake.nix
+++ flake.nix
@@ -44,11 +44,17 @@
         ./server
         ./droid
       ];
+
+      flake = {
+        homeManagerModules.default = import ./modules/home-manager.nix;
+      };
+
       systems = ["x86_64-linux" "aarch64-linux"];
       perSystem = {pkgs, ...}: {
         _module.args = {inherit self;};
 
         formatter = pkgs.alejandra;
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             alejandra
