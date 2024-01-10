b67dfb8ef47ac729a37aab4f5a20dcd8ddff5215
ErrorNoInternet <errornointernet@envs.net>
2024-01-11 00:45:32 +0800

    feat(profiles/development): add GOPATH

--- home/modules/profiles/development/default.nix
+++ home/modules/profiles/development/default.nix
@@ -18,22 +18,27 @@ in {
     };
 
   config = mkIf config.profiles.development.enable {
-    home.packages = with pkgs; [
-      (python3.withPackages (ps: with ps; [jedi]))
-      cachix
-      clang
-      clang-tools
-      gdb
-      gnumake
-      go
-      inputs.attic.packages.${pkgs.system}.attic
-      libllvm
-      linuxPackages_latest.perf
-      man-pages
-      python3Packages.bpython
-      rustup
-      scc
-      zig
-    ];
+    home = {
+      packages = with pkgs; [
+        (python3.withPackages (ps: with ps; [jedi]))
+        cachix
+        clang
+        clang-tools
+        gdb
+        gnumake
+        go
+        inputs.attic.packages.${pkgs.system}.attic
+        libllvm
+        linuxPackages_latest.perf
+        man-pages
+        python3Packages.bpython
+        rustup
+        scc
+        zig
+      ];
+      sessionVariables = {
+        GOPATH = "~/.go";
+      };
+    };
   };
 }
