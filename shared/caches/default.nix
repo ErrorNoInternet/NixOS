62cff846581c00da0335f8e3de3f08137e5a5dc8
ErrorNoInternet <errornointernet@envs.net>
2024-01-11 13:17:14 +0800

    fix: add back default substituter

--- shared/caches/default.nix
+++ shared/caches/default.nix
@@ -5,4 +5,9 @@
     ./nix-community.nix
     ./nix-gaming.nix
   ];
+
+  nix.settings = {
+    substituters = ["https://cache.nixos.org/"];
+    trusted-public-keys = ["cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="];
+  };
 }
