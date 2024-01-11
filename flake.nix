e6cd2bfa35a1fa90180e5c09b083db9a8b57eca7
ErrorNoInternet <errornointernet@envs.net>
2024-01-11 17:09:14 +0800

    chore(inputs): sort

--- flake.nix
+++ flake.nix
@@ -3,12 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     nix-on-droid = {
       url = "github:nix-community/nix-on-droid/release-23.05";
       inputs = {
@@ -16,24 +14,22 @@
         home-manager.follows = "home-manager";
       };
     };
-
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
     agenix.url = "github:ryantm/agenix";
-    attic.url = "github:zhaofengli/attic";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nix-colors.url = "github:misterio77/nix-colors";
-    nix-gaming.url = "github:fufexan/nix-gaming";
     nixvim.url = "github:nix-community/nixvim";
     spicetify-nix.url = "github:the-argus/spicetify-nix";
 
+    attic.url = "github:zhaofengli/attic";
     hyprland.url = "github:hyprwm/Hyprland";
     hyprpaper.url = "github:hyprwm/hyprpaper";
     hyprwm-contrib.url = "github:hyprwm/contrib";
     neovim.url = "github:neovim/neovim?dir=contrib";
+    nix-gaming.url = "github:fufexan/nix-gaming";
 
     shadower.url = "github:n3oney/shadower";
     yazi.url = "github:sxyazi/yazi";
