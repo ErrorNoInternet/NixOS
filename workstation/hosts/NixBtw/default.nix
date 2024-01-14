282faa50a5ab0d64840cbe3a1d1d990ba3db1b65
ErrorNoInternet <errornointernet@envs.net>
2024-01-14 19:20:32 +0800

    feat(NixBtw): automatically use latest (rc) kernel

--- workstation/hosts/NixBtw/default.nix
+++ workstation/hosts/NixBtw/default.nix
@@ -21,6 +21,17 @@
 
   workstation.desktops.hyprland.enable = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_testing;
   nix.gc.automatic = false;
+  boot.kernelPackages = with pkgs;
+    if
+      (builtins.compareVersions
+        linuxPackages_testing.kernel.version
+        linuxPackages_latest.kernel.version)
+      == -1
+    then linuxPackages_latest
+    else if
+      ((builtins.match "^([0-9]+\.[0-9]+)$" linuxPackages_latest.kernel.version)
+        == [linuxPackages_latest.kernel.version])
+    then linuxPackages_latest
+    else linuxPackages_testing;
 }
