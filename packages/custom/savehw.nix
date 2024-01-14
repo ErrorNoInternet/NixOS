ac7c0135678f324fe8e397025a1542c8d99cb77b
ErrorNoInternet <errornointernet@envs.net>
2024-01-15 01:21:32 +0800

    fix(savehw): pass shellcheck

--- packages/custom/savehw.nix
+++ packages/custom/savehw.nix
@@ -31,45 +31,46 @@ pkgs.writeShellApplication {
     folder="savehw_$(date +'%Y-%b-%d_%H-%M-%S' | tr '[:upper:]' '[:lower:]')"
     mkdir "$folder"
     cd "$folder"
+    mkdir dump
 
-    lscpu > lscpu 2>&1
-    lscpu -e > lscpu-extended 2>&1
-    cat /proc/cpuinfo > cpuinfo
-    sensors > sensors 2>&1
+    lscpu > dump/lscpu 2>&1
+    lscpu -e > dump/lscpu-extended 2>&1
+    cat /proc/cpuinfo > cpuinfo 2>&1
+    sensors > dump/sensors 2>&1
 
-    lsmem > lsmem 2>&1
-    cat /proc/meminfo > meminfo
+    lsmem > dump/lsmem 2>&1
+    cat /proc/meminfo > meminfo 2>&1  
 
-    lspci -vvv > lspci 2>&1
-    lsusb -v > lsusb 2>&1
-    lsblk -f > lsblk 2>&1
-    fdisk -l > fdisk 2>&1
-    cat /proc/scsi/scsi > scsi
+    lspci -vvv > dump/lspci 2>&1
+    lsusb -v > dump/lsusb 2>&1
+    lsblk -f > dump/lsblk 2>&1
+    fdisk -l > dump/fdisk 2>&1
+    cat /proc/scsi/scsi > scsi 2>&1
 
-    udevadm info -e > udevadm-info 2>&1
-    dmidecode > dmidecode 2>&1
+    udevadm info -e > dump/udevadm-info 2>&1
+    dmidecode > dump/dmidecode 2>&1
 
-    lshw > lshw 2>&1
-    lshw -short > lshw-short 2>&1
-    lshw -json > lshw-json 2>&1
+    lshw > dump/lshw 2>&1
+    lshw -short > dump/lshw-short 2>&1
+    lshw -json > dump/lshw-json 2>&1
 
-    inxi -iFv8 > inxi-iFv8 2>&1
-    inxi -iFv8 --output json --output-file "$(pwd)/inxi-iFv8-json" > inxi-iFv8-json.log 2>&1
-    inxi -iFv8 --output xml --output-file "$(pwd)/inxi-iFv8-xml" > inxi-iFv8-xml.log 2>&1
+    inxi -iFv8 > dump/inxi-iFv8 2>&1
+    inxi -iFv8 --output json --output-file "$(pwd)/inxi-iFv8-json" > dump/inxi-iFv8-json.log 2>&1
+    inxi -iFv8 --output xml --output-file "$(pwd)/inxi-iFv8-xml" > dump/inxi-iFv8-xml.log 2>&1
 
-    drm_info > drm_info 2>&1
-    drm_info -j > drm_info-json 2>&1
-    glxinfo > glxinfo 2>&1
+    drm_info > dump/drm_info 2>&1
+    drm_info -j > dump/drm_info-json 2>&1
+    glxinfo > dump/glxinfo 2>&1
 
-    eza --icons=always --no-permissions --no-user --no-time -lT /dev > dev 2>&1
-    eza --icons=always --no-permissions --no-user --no-time -lT /sys/class > sys-class 2>&1
+    eza --icons=always --no-permissions --no-user --no-time -lT /dev > dump/dev 2>&1
+    eza --icons=always --no-permissions --no-user --no-time -lT /sys/class > dump/sys-class 2>&1
 
     mkdir smartctl
     for device in $(lsblk -l -oNAME | tail -n+2); do
         smartctl -x "/dev/$device" > "smartctl/$device" 2>&1
     done
 
-    lsmod > lsmod 2>&1
+    lsmod > dump/lsmod 2>&1
 
     cd ..
     echo "done!"
