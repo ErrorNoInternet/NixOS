e389fac722f61723a7eacfa61246b22d6e8af173
ErrorNoInternet <errornointernet@envs.net>
2024-01-17 12:59:22 +0800

    fix(Rescanix): dynamically enable zfs

--- workstation/modules/zfs.nix
+++ workstation/modules/zfs.nix
@@ -11,6 +11,7 @@ in {
     boot = {
       kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
       supportedFilesystems = ["zfs"];
+
       zfs.forceImportRoot = false;
     };
   };
