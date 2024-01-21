bfc90290c3c9e6202147b0fdb3d27b5b751c97ee
ErrorNoInternet <errornointernet@envs.net>
2024-01-21 16:14:36 +0800

    feat(btop): toggle gpu graph

--- home/programs/terminal/btop.nix
+++ home/programs/terminal/btop.nix
@@ -13,8 +13,8 @@
       settings = {
         color_theme = "${lib.strings.toLower config.colors.schemeName}";
         net_iface = "wlp5s0";
-        nvml_measure_pcie_speeds = false;
         proc_sorting = "cpu direct";
+        show_gpu_info = "Off";
         swap_disk = false;
         theme_background = false;
         update_ms = 1000;
