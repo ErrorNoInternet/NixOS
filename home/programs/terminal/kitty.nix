cec82a2bd3d33d0e38a8f06f463f367c46752bb4
ErrorNoInternet <errornointernet@envs.net>
2024-01-23 17:29:43 +0800

    feat: add kitty back

---
+++ home/programs/terminal/kitty.nix
@@ -0,0 +1,56 @@
+{
+  config,
+  lib,
+  pkgs,
+  self,
+  ...
+}: {
+  config = lib.mkIf config.profiles.desktop.enable {
+    programs.kitty = {
+      enable = true;
+      package = self.legacyPackages.${pkgs.system}.kitty;
+      shellIntegration.enableFishIntegration = true;
+
+      font = {
+        inherit (config.font) name;
+        size = 9;
+      };
+      settings = {
+        background_opacity = "${builtins.toString config.opacity.normal}";
+        cursor_shape = "beam";
+        enable_audio_bell = false;
+        modify_font = "baseline 1";
+        shell = "tmux";
+      };
+      keybindings = {
+        "ctrl+shift+left" = "send_text all \\x1b\\x5b\\x31\\x3b\\x33\\x44";
+        "ctrl+shift+right" = "send_text all \\x1b\\x5b\\x31\\x3b\\x33\\x43";
+      };
+      extraConfig = with config.colors.scheme.colors; ''
+        url_color             #0087BD
+        selection_foreground  #000000
+        selection_background  #${base0A}
+        foreground            #${base04}
+        background            #${base00}
+        cursor                #${base0D}
+
+        color0   #${base01}
+        color8   #${base03}
+        color1   #${base08}
+        color9   #${base08}
+        color2   #${base0B}
+        color10  #${base0B}
+        color3   #${base0A}
+        color11  #${base0A}
+        color4   #${base0D}
+        color12  #${base0D}
+        color5   #${base0E}
+        color13  #${base0E}
+        color6   #${base0C}
+        color14  #${base07}
+        color7   #${base05}
+        color15  #${base06}
+      '';
+    };
+  };
+}
