8c29c292a6b885da38e35fd900136514e96e71af
ErrorNoInternet <errornointernet@envs.net>
2024-01-24 16:56:53 +0800

    fix(neovim/visual): fidget lsp server highlight

--- home/programs/terminal/neovim/visual.nix
+++ home/programs/terminal/neovim/visual.nix
@@ -50,7 +50,10 @@
         enable = true;
         extraOptions = {
           notification.window.winblend = 0;
-          progress.lsp.progress_ringbuf_size = 1024;
+          progress = {
+            display.group_style = "@boolean";
+            lsp.progress_ringbuf_size = 1024;
+          };
         };
       };
 
