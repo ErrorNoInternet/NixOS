efb76d114647daf2b0aa3c3c0788707674158e88
ErrorNoInternet <errornointernet@envs.net>
2024-01-26 16:13:51 +0800

    Revert "feat(hyprland): enable debug information"
    This reverts commit 92bc0cba32269da0dd2165e9479d8b372ffe7263.

--- home/programs/wayland/hyprland/default.nix
+++ home/programs/wayland/hyprland/default.nix
@@ -21,9 +21,7 @@
 
       wayland.windowManager.hyprland = {
         enable = true;
-        package = inputs.hyprland.packages.${pkgs.system}.hyprland.override {
-          debug = true;
-        };
+        package = inputs.hyprland.packages.${pkgs.system}.hyprland;
       };
     }
   );
