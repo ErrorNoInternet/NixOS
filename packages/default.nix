89ecf7d9e91b1db0f208291c51f5619b983bc401
ErrorNoInternet <errornointernet@envs.net>
2024-01-31 12:42:12 +0800

    feat(patches/wezterm): smithay 0.17 update

--- packages/default.nix
+++ packages/default.nix
@@ -58,7 +58,10 @@
       wezterm = inputs.wezterm.packages.${system}.default.overrideAttrs (oldAttrs: {
         patches =
           (oldAttrs.patches or [])
-          ++ [./patches/wezterm_optimized-build.patch];
+          ++ [
+            ./patches/wezterm_optimized-build.patch
+            ./patches/wezterm_smithay-0.17.patch
+          ];
       });
     };
   };
