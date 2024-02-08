4ab2125b77930ecf14f37493fac761856f258fcf
ErrorNoInternet <errornointernet@envs.net>
2024-02-08 17:54:51 +0800

    style: sort

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "ErrorNoInternet's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager";
