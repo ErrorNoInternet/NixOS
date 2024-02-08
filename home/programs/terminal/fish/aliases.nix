fe9cc88ebda5dda6c8ee7a6bcb8bcc956bd03e13
ErrorNoInternet <errornointernet@envs.net>
2024-02-08 14:45:07 +0800

    fix(fish/aliases): fastfetch configuration directory

--- home/programs/terminal/fish/aliases.nix
+++ home/programs/terminal/fish/aliases.nix
@@ -15,8 +15,8 @@
     ll = "ls -l";
     ls = "${lib.getExe pkgs.eza} --git --icons";
     lsimg = "${lib.getExe pkgs.timg} -ps --grid=6 --upscale --title --center --frames=1";
-    mf = "${lib.getExe pkgs.fastfetch} -c ~/${config.xdg.configHome}/fastfetch/minimal.jsonc";
-    mfd = "${lib.getExe pkgs.fastfetch} -c ~/${config.xdg.configHome}/fastfetch/nix-on-droid_minimal.jsonc";
+    mf = "${lib.getExe pkgs.fastfetch} -c ${config.xdg.configHome}/fastfetch/minimal.jsonc";
+    mfd = "${lib.getExe pkgs.fastfetch} -c ${config.xdg.configHome}/fastfetch/nix-on-droid_minimal.jsonc";
     nix = "nix --default-flake flake:nixpkgs";
     nom = "nom --default-flake flake:nixpkgs";
     py = "python3";
