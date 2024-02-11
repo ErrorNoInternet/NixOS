635415b865e88bbdf321641bf33792f2d9816587
ErrorNoInternet <errornointernet@envs.net>
2024-02-11 16:45:11 +0800

    refactor(waybar): remove redundant debug information options

--- home/programs/wayland/waybar.nix
+++ home/programs/wayland/waybar.nix
@@ -9,10 +9,7 @@
   config = lib.mkIf config.profiles.windowManager.enable {
     programs.waybar = with config.colors.scheme.palette; {
       enable = true;
-      package = inputs'.waybar.packages.default.overrideAttrs (_: {
-        dontStrip = true;
-        enableDebugging = true;
-      });
+      package = inputs'.waybar.packages.default;
 
       settings.mainBar = {
         position = "top";
