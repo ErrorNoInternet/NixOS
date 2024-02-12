4c10cf518dfdb8135d2a91afd6736af3391f0d93
ErrorNoInternet <errornointernet@envs.net>
2024-02-13 00:47:13 +0800

    feat(droid/locations/china): add back raw.githubusercontent.com host

--- droid/profiles/locations/china.nix
+++ droid/profiles/locations/china.nix
@@ -16,8 +16,8 @@ in {
       "https://mirror.sjtu.edu.cn/nix-channels/store"
     ];
     time.timeZone = "Asia/Shanghai";
-    # networking.extraHosts = ''
-    #   185.199.111.133 raw.githubusercontent.com
-    # '';
+    networking.extraHosts = ''
+      185.199.111.133 raw.githubusercontent.com
+    '';
   };
 }
