bc56beb93eb8deca0607f7f4be88675a934657bb
ErrorNoInternet <errornointernet@envs.net>
2024-02-13 18:54:14 +0800

    refactor(flake): remove followed inputs

--- flake.nix
+++ flake.nix
@@ -16,10 +16,7 @@
         home-manager.follows = "home-manager";
       };
     };
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-index-database.url = "github:nix-community/nix-index-database";
     agenix.url = "github:ryantm/agenix";
     nix-colors.url = "github:misterio77/nix-colors";
     nixvim.url = "github:nix-community/nixvim";
