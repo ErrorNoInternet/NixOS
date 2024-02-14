b1bcde6001898fea37d4fc5f336abe3d9fbd3447
ErrorNoInternet <errornointernet@envs.net>
2024-02-14 21:33:35 +0800

    refactor(hyprland/execs): minor clean up

--- home/programs/wayland/hyprland/execs.nix
+++ home/programs/wayland/hyprland/execs.nix
@@ -5,12 +5,15 @@
   ...
 }: {
   wayland.windowManager.hyprland.settings.exec-once = [
+    "hyprctl setcursor ${config.cursor.name} ${builtins.toString config.cursor.size}"
+
     "sleep 0.5 && ${lib.getExe pkgs.waybar}"
     (lib.mkIf config.wallpaper.enable "hyprpaper")
-    "hyprctl setcursor ${config.cursor.name} ${builtins.toString config.cursor.size}"
-    "swayidle -w timeout 570 'swaylock --grace 30 --fade-in 30' before-sleep 'swaylock --grace 0 --fade-in 1'"
-    "swayidle -w timeout 1200 'hyprctl dispatch dpms off'"
+
     "${lib.getExe pkgs.hyprland-autoname-workspaces}"
     "${pkgs.wl-clipboard}/bin/wl-paste --watch ${lib.getExe pkgs.cliphist} -max-items 1000 store"
+
+    "swayidle -w timeout 570 'swaylock --grace 30 --fade-in 30' before-sleep 'swaylock --grace 0 --fade-in 1'"
+    "swayidle -w timeout 1200 'hyprctl dispatch dpms off'"
   ];
 }
