cbfdef1fc733bfacf134b8ca8e16a872aa77ed77
ErrorNoInternet <errornointernet@envs.net>
2024-02-14 22:31:31 +0800

    refactor(secrets): use ++ syntax

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,11 +6,7 @@ let
     Crix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL8EVE+n6AqS8NWfZRjqLSI3HY+2YEWEsDo9oPSMt5C+";
     Pix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHoKZCpEfJ+rBCndkSlH3qEMypvSuJRdieSwPVMCKZB";
   };
-  all = [
-    users.NixBtw
-    systems.Crix
-    systems.Pix
-  ];
+  all = users ++ systems;
 in {
   "attic_server-token.age".publicKeys = [systems.Pix];
   "ddns.age".publicKeys = [users.NixBtw systems.Pix];
