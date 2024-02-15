15915fed71cf602ca303a87f5fe763e61de5330b
ErrorNoInternet <errornointernet@envs.net>
2024-02-16 02:08:37 +0800

    feat(fish/abbreviations/nix): common nix build flags

--- home/programs/terminal/fish/abbreviations/nix.nix
+++ home/programs/terminal/fish/abbreviations/nix.nix
@@ -1,12 +1,33 @@
 {
   programs.fish.shellAbbrs = {
-    "-insecure" = {
+    "/insecure" = {
       position = "anywhere";
       expansion = "NIXPKGS_ALLOW_INSECURE=1";
     };
-    "-unfree" = {
+    "/unfree" = {
       position = "anywhere";
       expansion = "NIXPKGS_ALLOW_UNFREE=1";
+    };
+
+    "/es" = {
+      position = "anywhere";
+      expansion = "--extra-substituters";
+    };
+    "/etpk" = {
+      position = "anywhere";
+      expansion = "--extra-trusted-public-keys";
+    };
+    "/s" = {
+      position = "anywhere";
+      expansion = "--substituters";
+    };
+    "/tpk" = {
+      position = "anywhere";
+      expansion = "--trusted-public-keys";
+    };
+    "/ncnt" = {
+      position = "anywhere";
+      expansion = "--narinfo-cache-negative-ttl 0";
     };
 
     nb = "nix build";
