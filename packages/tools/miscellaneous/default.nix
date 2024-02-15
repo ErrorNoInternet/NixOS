8d3be1fc1ddb03212ac188b37e7f6c8799bb1a14
ErrorNoInternet <errornointernet@envs.net>
2024-02-16 03:20:47 +0800

    refactor(packages): clean up scripts

--- packages/tools/miscellaneous/default.nix
+++ packages/tools/miscellaneous/default.nix
@@ -8,6 +8,10 @@
     packages = with pkgs; {
       attic = callPackage ./attic {inherit inputs';};
       hwatch = callPackage ./hwatch {};
+      passgen = callPackage ./passgen.nix {};
+      savehw = callPackage ./savehw.nix {};
+      tbw = callPackage ./tbw.nix {};
+
       openrgb = openrgb.withPlugins [openrgb-plugin-effects];
       openrgb-libusb = callPackage ./openrgb {};
     };
--- packages/tools/scripts/default.nix
+++ packages/tools/scripts/default.nix
@@ -4,10 +4,7 @@
     packages = with pkgs; {
       brightness = callPackage ./brightness.nix {};
       hyprgamemode = callPackage ./hyprgamemode.nix {};
-      passgen = callPackage ./passgen.nix {};
       pavolume = callPackage ./pavolume.nix {};
-      savehw = callPackage ./savehw.nix {};
-      tbw = callPackage ./tbw.nix {};
     };
   };
 }
