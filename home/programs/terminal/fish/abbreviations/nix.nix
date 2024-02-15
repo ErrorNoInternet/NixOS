94ec77f4e2c51817e87568c6b59ee52b596c2eb4
ErrorNoInternet <errornointernet@envs.net>
2024-02-15 21:10:36 +0800

    feat(fish/abbreviations/nix): add nixpkgs environment variables

--- home/programs/terminal/fish/abbreviations/nix.nix
+++ home/programs/terminal/fish/abbreviations/nix.nix
@@ -1,5 +1,14 @@
 {
   programs.fish.shellAbbrs = {
+    "-insecure" = {
+      position = "anywhere";
+      expansion = "NIXPKGS_ALLOW_INSECURE=1";
+    };
+    "-unfree" = {
+      position = "anywhere";
+      expansion = "NIXPKGS_ALLOW_UNFREE=1";
+    };
+
     nb = "nix build";
     nd = "nix develop -c fish";
     nr = "nix run";
