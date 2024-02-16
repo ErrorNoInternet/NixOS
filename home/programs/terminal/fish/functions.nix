884655957aec7141e73724783e3a09ab7e27ca38
ErrorNoInternet <errornointernet@envs.net>
2024-02-16 15:24:54 +0800

    feat(fish/functions): add glfzf

--- home/programs/terminal/fish/functions.nix
+++ home/programs/terminal/fish/functions.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   programs.fish.interactiveShellInit = ''
     function toggle-comment
       set cursor (commandline --cursor)
@@ -68,6 +72,14 @@
 
     function ggr -d "fancy git history graph"
       ${pkgs.git-graph}/bin/git-graph --color always -s ascii --no-pager $argv | ${pkgs.less}/bin/less
+    end
+
+    function glfzf -d "use fzf to preview git commits"
+      git log --graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" $argv | \
+        ${lib.getExe pkgs.fzf} --ansi --no-sort --reverse --tiebreak=index --preview-window=right:60% \
+          --preview 'function preview; set commit (echo $argv | grep -o "[a-f0-9]\{7\}"); git show --color=always $commit | ${lib.getExe pkgs.delta} --width=(tput cols); end; preview {}' \
+          --bind "j:down,k:up,alt-j:preview-down,alt-k:preview-up,shift-down:preview-page-down,shift-up:preview-page-up,q:abort,ctrl-m:execute:
+                  function show; set commit (echo \$argv | grep -o '[a-f0-9]\{7\}'); git show --color=always \$commit | ${lib.getExe pkgs.delta} --width=(tput cols) | less -R; end; show {}"
     end
 
 
