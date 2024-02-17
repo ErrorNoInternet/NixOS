7823f854c291cf3fb63ac484edddccdd879370b0
ErrorNoInternet <errornointernet@envs.net>
2024-02-17 14:03:43 +0800

    fix(server): helpers -> lib

--- server/default.nix
+++ server/default.nix
@@ -36,7 +36,7 @@
         ];
       });
 in {
-  flake.nixosConfigurations = with self.helpers.systems; {
+  flake.nixosConfigurations = with self.lib.systems; {
     Crix = mkServer "Crix" "x86_64-linux";
     Pix = mkHmServer "Pix" "aarch64-linux";
   };
