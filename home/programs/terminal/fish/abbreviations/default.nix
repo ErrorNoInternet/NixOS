8190a6c65c27f62a98671b69a33a7b616967c998
ErrorNoInternet <errornointernet@envs.net>
2024-02-17 21:52:08 +0800

    feat(fish/abbreviations): add filename to wf-recorder

--- home/programs/terminal/fish/abbreviations/default.nix
+++ home/programs/terminal/fish/abbreviations/default.nix
@@ -15,6 +15,6 @@
     nv = "nvim";
 
     clo = "curl -LO";
-    wr = "wf-recorder -b 0 -c h264_nvenc -p b=5M";
+    wr = "wf-recorder -b 0 -c h264_nvenc -p b=5M -f recording_(date \"+%Y-%m-%-d_%H:%M:%S\").mp4";
   };
 }
