9378ae55581e9b798f69e855a2694f459d17d28c
ErrorNoInternet <errornointernet@envs.net>
2024-02-19 19:27:18 +0800

    refactor(Rescanix/hardware): minor clean up

--- workstation/hosts/Rescanix/hardware.nix
+++ workstation/hosts/Rescanix/hardware.nix
@@ -9,49 +9,50 @@
   ];
 
   boot = {
-    initrd.availableKernelModules = [
-      "ahci"
-      "ehci_pci"
-      "rtsx_pci_sdmmc"
-      "sd_mod"
-      "sr_mod"
-      "usb_storage"
-      "virtio_blk"
-      "virtio_pci"
-      "xhci_pci"
-    ];
-    initrd.kernelModules = [];
+    initrd = {
+      availableKernelModules = [
+        "ahci"
+        "ehci_pci"
+        "rtsx_pci_sdmmc"
+        "sd_mod"
+        "sr_mod"
+        "usb_storage"
+        "virtio_blk"
+        "virtio_pci"
+        "xhci_pci"
+      ];
+      kernelModules = [];
+    };
     kernelModules = ["kvm-intel"];
     extraModulePackages = [];
   };
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/69696969-6969-6969-6969-696969696969";
+      device = "/dev/disk/by-uuid/10101010-1010-1010-1010-101010101010";
       fsType = "btrfs";
       options = ["subvol=@"];
     };
     "/home" = {
-      device = "/dev/disk/by-uuid/69696969-6969-6969-6969-696969696969";
+      device = "/dev/disk/by-uuid/10101010-1010-1010-1010-101010101010";
       fsType = "btrfs";
       options = ["subvol=@home"];
     };
     "/nix" = {
-      device = "/dev/disk/by-uuid/69696969-6969-6969-6969-696969696969";
+      device = "/dev/disk/by-uuid/10101010-1010-1010-1010-101010101010";
       fsType = "btrfs";
       options = ["subvol=@nix"];
     };
     "/boot" = {
-      device = "/dev/disk/by-uuid/6969-6969";
+      device = "/dev/disk/by-uuid/1010-1010";
       fsType = "vfat";
     };
   };
-
   swapDevices = [];
 
   networking.useDHCP = lib.mkDefault true;
 
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
   time.hardwareClockInLocalTime = true;
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
