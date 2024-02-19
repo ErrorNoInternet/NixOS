75d1a7fc4ba024e6d97895e3aa88b48857cfcb3b
ErrorNoInternet <errornointernet@envs.net>
2024-02-19 13:12:51 +0800

    fix(packages): remove darwin

--- packages/plugins/neovim/default.nix
+++ packages/plugins/neovim/default.nix
@@ -1,10 +1,5 @@
 {
-  systems = [
-    "aarch64-darwin"
-    "aarch64-linux"
-    "x86_64-darwin"
-    "x86_64-linux"
-  ];
+  systems = ["aarch64-linux" "x86_64-linux"];
   perSystem = {pkgs, ...}: {
     packages = with pkgs; {
       vim-plugin-textobj-indent = callPackage ./textobj-indent.nix {};
