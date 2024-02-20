b9404980ee1e80a38d93358fc257ec9119598b96
ErrorNoInternet <errornointernet@envs.net>
2024-02-20 12:38:27 +0800

    feat(hyprland): re-enable mouse_move_enables_dpms

--- home/programs/wayland/hyprland/options.nix
+++ home/programs/wayland/hyprland/options.nix
@@ -26,9 +26,7 @@
       disable_hyprland_logo = true;
       disable_splash_rendering = true;
       key_press_enables_dpms = true;
-
-      # TODO: enable when https://github.com/hyprwm/Hyprland/issues/4670 is fixed
-      mouse_move_enables_dpms = false;
+      mouse_move_enables_dpms = true;
     };
 
     dwindle = {
