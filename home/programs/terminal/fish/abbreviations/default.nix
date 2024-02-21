2f6e437863dcc4eb78b26fdb2f3bab9dc78c714d
ErrorNoInternet <errornointernet@envs.net>
2024-02-21 20:39:49 +0800

    feat(fish/abbreviations): add echo and printf

--- home/programs/terminal/fish/abbreviations/default.nix
+++ home/programs/terminal/fish/abbreviations/default.nix
@@ -11,8 +11,10 @@
     ".........." = "../../../../..";
     "............" = "../../../../../..";
 
-    s = "sudo";
+    e = "echo";
     nv = "nvim";
+    p = "printf";
+    s = "sudo";
 
     clo = "curl -LO";
     wr = "wf-recorder -b 0 -c h264_nvenc -p b=5M -f recording_(date \"+%Y-%m-%-d_%H:%M:%S\").mp4";
