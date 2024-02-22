b362395e018f567f3bb94e749250ad4f2cb3625d
ErrorNoInternet <errornointernet@envs.net>
2024-02-22 21:29:22 +0800

    refactor(workstation/specialisations/no-nvidia): minor clean up

--- workstation/specialisations/no-nvidia.nix
+++ workstation/specialisations/no-nvidia.nix
@@ -6,10 +6,9 @@
 }: let
   cfg = config.workstation.modules.nvidia;
 in {
-  specialisation = with self.lib.systems;
-    lib.mkIf cfg.enable {
-      no-nvidia = mkSpecialisation "no-nvidia" {
-        workstation.modules.nvidia.enable = false;
-      };
+  specialisation = lib.mkIf cfg.enable {
+    no-nvidia = self.lib.systems.mkSpecialisation "no-nvidia" {
+      workstation.modules.nvidia.enable = false;
     };
+  };
 }
