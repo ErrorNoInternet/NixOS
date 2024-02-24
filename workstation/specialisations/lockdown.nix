2cd6518144045bedd39a3c093f63571c34306421
ErrorNoInternet <errornointernet@envs.net>
2024-02-24 21:19:45 +0800

    feat(workstation/specialisations/lockdown): disable bluetooth and lock kernel modules

--- workstation/specialisations/lockdown.nix
+++ workstation/specialisations/lockdown.nix
@@ -3,10 +3,16 @@
     boot.kernel.sysctl = {
       "kernel.sysrq" = 0;
     };
+
+    hardware.bluetooth.enable = false;
+
     networking.firewall = {
       enable = true;
       allowPing = false;
     };
+
+    security.lockKernelModules = true;
+
     services.openssh.enable = false;
   };
 }
