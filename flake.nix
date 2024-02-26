d7ed97d82f2ea038daf5a16525b9dc91e819faea
ErrorNoInternet <errornointernet@envs.net>
2024-02-26 13:30:38 +0800

    refactor(inputs): follow main nixpkgs
    To speed up evaluation, lower memory usage, and to use up less space
    (each nixpkgs checkout is like 35 MB).

--- flake.nix
+++ flake.nix
@@ -2,37 +2,120 @@
   description = "ErrorNoInternet's NixOS configuration";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     nix-on-droid = {
       url = "github:nix-community/nix-on-droid";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.home-manager.follows = "home-manager";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
     };
-    agenix.url = "github:ryantm/agenix";
-    nix-colors.url = "github:misterio77/nix-colors";
 
-    attic.url = "github:zhaofengli/attic";
-    hsize.url = "github:ErrorNoInternet/hsize";
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprpaper.url = "github:hyprwm/hyprpaper";
-    hyprwm-contrib.url = "github:hyprwm/contrib";
-    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
-    nix-gaming.url = "github:fufexan/nix-gaming";
-    nix-index-database.url = "github:nix-community/nix-index-database";
-    nix-super.url = "github:privatevoid-net/nix-super";
-    nixvim.url = "github:nix-community/nixvim";
-    overmask.url = "github:ErrorNoInternet/overmask";
-    paralload.url = "github:ErrorNoInternet/Paralload";
-    shadower.url = "github:n3oney/shadower";
-    spicetify-nix.url = "github:the-argus/spicetify-nix";
-    waybar.url = "github:Alexays/Waybar";
-    wezterm.url = "github:wez/wezterm?dir=nix";
-    yazi.url = "github:sxyazi/yazi";
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-colors = {
+      url = "github:misterio77/nix-colors";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    attic = {
+      url = "github:zhaofengli/attic";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hsize = {
+      url = "github:ErrorNoInternet/hsize";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprpaper = {
+      url = "github:hyprwm/hyprpaper";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprwm-contrib = {
+      url = "github:hyprwm/contrib";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    neovim-nightly = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-gaming = {
+      url = "github:fufexan/nix-gaming";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-super = {
+      url = "github:privatevoid-net/nix-super";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    overmask = {
+      url = "github:ErrorNoInternet/overmask";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    paralload = {
+      url = "github:ErrorNoInternet/Paralload";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    shadower = {
+      url = "github:n3oney/shadower";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    spicetify-nix = {
+      url = "github:the-argus/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    waybar = {
+      url = "github:Alexays/Waybar";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    wezterm = {
+      url = "github:wez/wezterm?dir=nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    yazi = {
+      url = "github:sxyazi/yazi";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {flake-parts, ...} @ inputs:
