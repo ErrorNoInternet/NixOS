d2d7b5dfdb78babd144d1066d4fa1d2c3dfb342e
ErrorNoInternet <errornointernet@envs.net>
2024-02-26 16:45:12 +0800

    feat(fish/abbreviations): add systemctl commands

--- home/programs/terminal/fish/abbreviations/default.nix
+++ home/programs/terminal/fish/abbreviations/default.nix
@@ -16,6 +16,8 @@
     nv = "nvim";
     p = "printf";
     s = "sudo";
+    sc = "systemctl";
+    scu = "systemctl --user";
 
     clo = "curl -LO";
     wr = "wf-recorder -b 0 -c h264_nvenc -p b=5M -f recording_(date \"+%Y-%m-%-d_%H:%M:%S\").mp4";
