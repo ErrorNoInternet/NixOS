bbe0b586a3a0f3a79d70c28904b2381cb353cc9c
ErrorNoInternet <errornointernet@envs.net>
2024-02-27 17:44:57 +0800

    refactor(btop): exclude extra directories

--- home/programs/terminal/btop.nix
+++ home/programs/terminal/btop.nix
@@ -8,6 +8,7 @@
       enable = true;
       settings = {
         color_theme = "${lib.strings.toLower config.colors.schemeName}";
+        disks_filter = "exclude=/nix /nix/store /home";
         net_iface = "wlp5s0";
         proc_sorting = "cpu direct";
         show_gpu_info = "Off";
