337464ce84e6d26d119c77cadd99acfdcb02add0
ErrorNoInternet <errornointernet@envs.net>
2024-02-28 15:42:29 +0800

    feat(fish/abbreviations/zfs): add verbose flags

--- home/programs/terminal/fish/abbreviations/zfs.nix
+++ home/programs/terminal/fish/abbreviations/zfs.nix
@@ -6,10 +6,12 @@
     zfl = "zfs list";
     zfls = "zfs list -t snapshot";
     zpl = "zpool list";
+    zplv = "zpool list -v";
     zps = "zpool status";
+    zpsv = "zpool status -v";
 
     zflk = "zfs load-key -a";
     zfm = "zfs mount -a";
-    zpi = "sudo zpool import -al";
+    zpi = "zpool import -al";
   };
 }
