4e276a182e8c154051fccaa12b4147b54274d5e5
ErrorNoInternet <errornointernet@envs.net>
2024-02-29 10:04:38 +0800

    feat(fish/abbreviations/zfs): add get commands

--- home/programs/terminal/fish/abbreviations/zfs.nix
+++ home/programs/terminal/fish/abbreviations/zfs.nix
@@ -3,8 +3,11 @@
     zf = "zfs";
     zp = "zpool";
 
+    zfg = "zfs get";
+    zfgf = "zfs get -t filesystem";
     zfl = "zfs list";
     zfls = "zfs list -t snapshot";
+    zpg = "zpool get";
     zpl = "zpool list";
     zplv = "zpool list -v";
     zps = "zpool status";
