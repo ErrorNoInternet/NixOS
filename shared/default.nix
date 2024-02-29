33c0d2b71a266034e29e36c3be6159e785e33786
ErrorNoInternet <errornointernet@envs.net>
2024-02-29 15:25:50 +0800

    refactor: harden sysctl parameters
    https://wiki.archlinux.org/title/sysctl

--- shared/default.nix
+++ shared/default.nix
@@ -42,6 +42,15 @@ in {
     };
   };
 
+  boot.kernel.sysctl = {
+    "net.ipv4.conf.all.accept_redirects" = 0;
+    "net.ipv4.conf.all.secure_redirects" = 0;
+    "net.ipv4.conf.default.accept_redirects" = 0;
+    "net.ipv4.conf.default.secure_redirects" = 0;
+    "net.ipv6.conf.all.accept_redirects" = 0;
+    "net.ipv6.conf.default.accept_redirects" = 0;
+  };
+
   zramSwap = {
     enable = true;
     memoryPercent = 200;
--- workstation/specialisations/lockdown.nix
+++ workstation/specialisations/lockdown.nix
@@ -1,7 +1,11 @@
 {self, ...}: {
   specialisation.lockdown = self.lib.systems.mkSpecialisation "lockdown" {
     boot.kernel.sysctl = {
+      "kernel.kexec_load_disabled" = 1;
       "kernel.sysrq" = 0;
+
+      "net.ipv4.conf.all.rp_filter" = 1;
+      "net.ipv4.conf.default.rp_filter" = 1;
     };
 
     networking.firewall = {
