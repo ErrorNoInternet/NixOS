9393e244452ed83b0df493f6b72bb17b58a1f7f6
ErrorNoInternet <errornointernet@envs.net>
2024-03-02 01:23:26 +0800

    feat(workstation/specialisations/lockdown): use hardened kernel & disable services

--- workstation/specialisations/lockdown.nix
+++ workstation/specialisations/lockdown.nix
@@ -1,16 +1,29 @@
-{self, ...}: {
+{
+  pkgs,
+  self,
+  ...
+}: {
   specialisation.lockdown = self.lib.systems.mkSpecialisation "lockdown" {
-    boot.kernel.sysctl = {
-      "kernel.kexec_load_disabled" = 1;
-      "kernel.sysrq" = 0;
+    boot = {
+      kernelPackages = pkgs.linuxPackages_hardened;
 
-      "net.ipv4.conf.all.rp_filter" = 1;
-      "net.ipv4.conf.default.rp_filter" = 1;
+      kernel.sysctl = {
+        "kernel.kexec_load_disabled" = 1;
+        "kernel.sysrq" = 0;
+
+        "net.ipv4.conf.all.rp_filter" = 1;
+        "net.ipv4.conf.default.rp_filter" = 1;
+      };
     };
 
     security.lockKernelModules = true;
 
-    services.logind.lidSwitch = "suspend";
+    services = {
+      logind.lidSwitch = "suspend";
+
+      openssh.enable = false;
+      rpcbind.enable = false;
+    };
 
     networking.firewall = {
       enable = true;
