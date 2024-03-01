7b787ca39dfea18e87ef91a89a31ed291dfdeb6a
ErrorNoInternet <errornointernet@envs.net>
2024-03-01 09:19:00 +0800

    feat(workstation): add basic plasma6 module

--- workstation/programs/desktops/plasma5.nix
+++ workstation/programs/desktops/plasma5.nix
@@ -3,11 +3,12 @@
   lib,
   ...
 }: let
+  cfg = config.workstation.desktops.plasma5;
   inherit (lib) mkEnableOption mkIf;
 in {
   options.workstation.desktops.plasma5.enable = mkEnableOption "";
 
-  config = mkIf config.workstation.desktops.plasma5.enable {
+  config = mkIf cfg.enable {
     services.xserver.desktopManager.plasma5.enable = true;
   };
 }
