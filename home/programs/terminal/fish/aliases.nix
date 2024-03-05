31befca6188c60407063b55f72be0603d3629ea6
ErrorNoInternet <errornointernet@envs.net>
2024-03-05 17:15:30 +0800

    refactor(fish/aliases): remove duplicate packages

--- home/programs/terminal/fish/aliases.nix
+++ home/programs/terminal/fish/aliases.nix
@@ -1,44 +1,35 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}: {
-  programs.fish.shellAliases = with lib; let
-    nh = "${getExe pkgs.nh}";
-    playerctl = "${getExe pkgs.playerctl}";
-    timg = "${getExe pkgs.timg}";
-  in {
+{config, ...}: {
+  programs.fish.shellAliases = {
     H = "exec Hyprland";
 
-    cm = "${pkgs.cmatrix}/bin/cmatrix -C blue";
-    drg = "${lib.getExe pkgs.ripdrag} -xa";
+    cm = "cmatrix -C blue";
+    drg = "ripdrag -xa";
     grep = "grep --color";
     ip = "ip --color";
     l = "ls -l";
     la = "ls -la";
     ll = "ls -l";
-    ls = "${lib.getExe pkgs.eza} --git --icons";
-    lsimg = "${timg} -pk --grid=6 --upscale --title --center --frames=1";
+    ls = "eza --git --icons";
+    lsimg = "timg -pk --grid=6 --upscale --title --center --frames=1";
     py = "python3";
     q = "exit";
     qq = "exit";
-    timg = "${timg} -pk";
+    timg = "timg -pk";
 
     ff = "fastfetch";
     f = "fastfetch -c ${config.xdg.configHome}/fastfetch/minimal.jsonc";
     droidf = "fastfetch -c ${config.xdg.configHome}/fastfetch/minimal-droid.jsonc";
 
-    B = "${nh} os boot .";
+    B = "nh os boot .";
     Ba = "B -a";
-    S = "${nh} os switch .";
+    S = "nh os switch .";
     Sa = "S -a";
-    T = "${nh} os test .";
+    T = "nh os test .";
     Ta = "T -a";
 
-    pp = "${playerctl} play-pause";
-    ppc = "${playerctl} play";
-    pps = "${playerctl} pause";
-    pwdc = "pwd | tr -d '\\n' | ${pkgs.wl-clipboard}/bin/wl-copy";
+    pp = "playerctl play-pause";
+    ppc = "playerctl play";
+    pps = "playerctl pause";
+    pwdc = "pwd | tr -d '\\n' | wl-copy";
   };
 }
