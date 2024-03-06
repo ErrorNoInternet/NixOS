70fa007f1c7280bdfa5d399e934941ba843903ec
ErrorNoInternet <errornointernet@envs.net>
2024-03-06 18:13:05 +0800

    feat(droid): map flake registries

--- droid/common.nix
+++ droid/common.nix
@@ -1,4 +1,5 @@
 {
+  inputs,
   lib,
   pkgs,
   self',
@@ -19,6 +20,15 @@
   environment.motd = "";
   nix = {
     package = self'.packages.nix;
+
+    nixPath = ["nixpkgs=${inputs.nixpkgs}"];
+    registry = let
+      mappedRegistry = lib.mapAttrs' (name: flake:
+        lib.nameValuePair name {inherit flake;})
+      inputs;
+    in
+      mappedRegistry // {default = mappedRegistry.nixpkgs;};
+
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
