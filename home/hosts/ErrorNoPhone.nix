11541dbb60d0aaf7100ca938127b7e6f2b26b98e
ErrorNoInternet <errornointernet@envs.net>
2024-03-06 22:48:26 +0800

    fix(ErrorNoPhone/nb): fix nb storage path

--- home/hosts/ErrorNoPhone.nix
+++ home/hosts/ErrorNoPhone.nix
@@ -1,9 +1,9 @@
-{config, ...}: {
+{
   customPrograms.terminal = {
     git.enable = true;
     nb = {
       enable = true;
-      dataDirectory = "/data/data/com.termux.nix/files/home/${config.home.username}/.nb";
+      dataDirectory = "/data/data/com.termux.nix/files/home/.nb";
     };
   };
 }
