bf3c8fc11e91d3c52804f16294f2926f0ea0bdc2
ErrorNoInternet <errornointernet@envs.net>
2024-03-08 14:22:51 +0800

    refactor(fish/abbreviations/wf-recorder): add audio

--- home/programs/terminal/fish/abbreviations/default.nix
+++ home/programs/terminal/fish/abbreviations/default.nix
@@ -6,30 +6,37 @@
     ./zfs.nix
   ];
 
-  programs.fish.shellAbbrs = {
-    "...." = "../..";
-    "......" = "../../..";
-    "........" = "../../../..";
-    ".........." = "../../../../..";
-    "............" = "../../../../../..";
+  programs.fish.shellAbbrs =
+    {
+      "...." = "../..";
+      "......" = "../../..";
+      "........" = "../../../..";
+      ".........." = "../../../../..";
+      "............" = "../../../../../..";
 
-    c = "cd ${
-      if config.flags.nixOnDroid
-      then "~/.config/nixpkgs"
-      else "/etc/nixos"
-    }/configuration.nix/";
+      c = "cd ${
+        if config.flags.nixOnDroid
+        then "~/.config/nixpkgs"
+        else "/etc/nixos"
+      }/configuration.nix/";
 
-    "-" = "cd -";
-    cl = "curl -L";
-    clo = "curl -LO";
-    e = "echo";
-    nv = "nvim";
-    p = "printf";
-    s = "sudo";
-    sc = "systemctl";
-    scu = "systemctl --user";
-    sl = "sleep";
-    wr = "wf-recorder -b 0 -c h264_nvenc -p b=5M -f recording_(date \"+%Y-%m-%-d_%H:%M:%S\").mp4";
-    wrs = "wf-recorder -b 0 -c h264_nvenc -p b=5M -f recording_(date \"+%Y-%m-%-d_%H:%M:%S\").mp4 -g (slurp)";
-  };
+      "-" = "cd -";
+      cl = "curl -L";
+      clo = "curl -LO";
+      e = "echo";
+      nv = "nvim";
+      p = "printf";
+      s = "sudo";
+      sc = "systemctl";
+      scu = "systemctl --user";
+      sl = "sleep";
+    }
+    // (let
+      wf-recorder = "wf-recorder -b 0 -c h264_nvenc -p b=5M -f recording_(date \"+%Y-%m-%d_%H:%M:%S\").mp4";
+    in {
+      wr = "${wf-recorder}";
+      wra = "${wf-recorder} -a=alsa_output.pci-0000_00_1b.0.analog-stereo.monitor";
+      wrs = "${wf-recorder} -g (slurp)";
+      wrsa = "${wf-recorder} -a=alsa_output.pci-0000_00_1b.0.analog-stereo.monitor -g (slurp)";
+    });
 }
