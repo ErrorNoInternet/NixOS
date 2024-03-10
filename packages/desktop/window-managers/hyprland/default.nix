5b9d908b4588b245a267002d6ef9bb16e57e196f
ErrorNoInternet <errornointernet@envs.net>
2024-03-10 22:56:40 +0800

    feat(packages/hyprland): remove xdg size predictions
    WezTerm does not run after Hyprland commit f4f3aa2e505ca1b22688fb4a32f31c4bfb851dd0.

--- packages/desktop/window-managers/hyprland/default.nix
+++ packages/desktop/window-managers/hyprland/default.nix
@@ -5,7 +5,7 @@
 }:
 inputs'.hyprland.packages.hyprland.overrideAttrs (_: {
   prePatch = ''
-    git apply ${./remove-wallpapers.patch}
+    git apply ${./remove-wallpapers.patch} ${./remove-size-predictions.patch}
   '';
   inherit enableXWayland enableNvidiaPatches;
 })
