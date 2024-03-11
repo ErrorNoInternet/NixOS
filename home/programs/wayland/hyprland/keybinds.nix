c8fd09790ed5b28d76d4f1d46fada7df6304cb8c
ErrorNoInternet <errornointernet@envs.net>
2024-03-11 15:54:39 +0800

    refactor(hyprland): generate more with nix

--- home/programs/wayland/hyprland/keybinds.nix
+++ home/programs/wayland/hyprland/keybinds.nix
@@ -13,86 +13,76 @@
     shadower = "${inputs'.shadower.packages.shadower}/bin/shadower -r14";
   in {
     settings = {
-      bind = [
-        "$mod CTRL SHIFT, code:22, exec, hyprctl dispatch exit"
-        "$mod, escape, exec, wleave -p layer-shell -f -b5 -T425 -B425 -L250 -R250"
+      bind =
+        [
+          "$mod CTRL SHIFT, code:22, exec, hyprctl dispatch exit"
+          "$mod, escape, exec, wleave -p layer-shell -f -b5 -T425 -B425 -L250 -R250"
 
-        "$mod, D, exec, ${rofi} || pkill rofi"
-        "$mod, Z, exec, ${rofi} || pkill rofi"
-        "$mod, G, exec, cliphist list | (rofi -dmenu || pkill rofi) | cliphist decode | wl-copy"
-        "$mod, code:60, exec, rofi -show emoji || pkill rofi"
+          "$mod, D, exec, ${rofi} || pkill rofi"
+          "$mod, Z, exec, ${rofi} || pkill rofi"
+          "$mod, G, exec, cliphist list | (rofi -dmenu || pkill rofi) | cliphist decode | wl-copy"
+          "$mod, code:60, exec, rofi -show emoji || pkill rofi"
 
-        ", PRINT, exec, ${grimblast} | wl-copy"
-        "SHIFT, PRINT, exec, ${grimblast} | ${shadower} | wl-copy"
-        "CTRL, PRINT, exec, ${grimblast} | ${satty}"
-        "CTRL SHIFT, PRINT, exec, ${grimblast} | ${shadower} | ${satty} | wl-copy"
+          ", PRINT, exec, ${grimblast} | wl-copy"
+          "SHIFT, PRINT, exec, ${grimblast} | ${shadower} | wl-copy"
+          "CTRL, PRINT, exec, ${grimblast} | ${satty}"
+          "CTRL SHIFT, PRINT, exec, ${grimblast} | ${shadower} | ${satty} | wl-copy"
 
-        "$mod, F1, exec, ${hyprgamemode}"
-        "$mod, L, exec, swaylock --grace 0 --fade-in 1"
-        "$mod, A, exec, scratchpad"
-        "$mod SHIFT, A, exec, scratchpad -g"
-        "$mod, O, exec, swaync-client -t"
-        "$mod, RETURN, exec, ${config.terminal.command}"
-        "$mod, code:51, exec, firefox"
-        "$mod SHIFT, RETURN, exec, [float; center] ${config.terminal.command}"
+          "$mod, F1, exec, ${hyprgamemode}"
+          "$mod, L, exec, swaylock --grace 0 --fade-in 1"
+          "$mod, A, exec, scratchpad"
+          "$mod SHIFT, A, exec, scratchpad -g"
+          "$mod, O, exec, swaync-client -t"
+          "$mod, RETURN, exec, ${config.terminal.command}"
+          "$mod, code:51, exec, firefox"
+          "$mod SHIFT, RETURN, exec, [float; center] ${config.terminal.command}"
 
-        "$mod SHIFT, F, fullscreen"
-        "$mod, F, fullscreen, 1"
-        "$mod, V, togglefloating"
-        "$mod, W, togglesplit"
-        "$mod, T, togglegroup"
-        "$mod SHIFT, Q, killactive"
+          "$mod SHIFT, F, fullscreen"
+          "$mod, F, fullscreen, 1"
+          "$mod, V, togglefloating"
+          "$mod, W, togglesplit"
+          "$mod, T, togglegroup"
+          "$mod SHIFT, Q, killactive"
 
-        "$mod, left, movefocus, l"
-        "$mod, right, movefocus, r"
-        "$mod, up, movefocus, u"
-        "$mod, down, movefocus, d"
+          "$mod, left, movefocus, l"
+          "$mod, right, movefocus, r"
+          "$mod, up, movefocus, u"
+          "$mod, down, movefocus, d"
 
-        "$mod SHIFT, left, movewindow, l"
-        "$mod SHIFT, right, movewindow, r"
-        "$mod SHIFT, up, movewindow, u"
-        "$mod SHIFT, down, movewindow, d"
-        "$mod SHIFT, H, movewindow, l"
-        "$mod SHIFT, L, movewindow, r"
-        "$mod SHIFT, K, movewindow, u"
-        "$mod SHIFT, J, movewindow, d"
+          "$mod SHIFT, left, movewindow, l"
+          "$mod SHIFT, right, movewindow, r"
+          "$mod SHIFT, up, movewindow, u"
+          "$mod SHIFT, down, movewindow, d"
+          "$mod SHIFT, H, movewindow, l"
+          "$mod SHIFT, L, movewindow, r"
+          "$mod SHIFT, K, movewindow, u"
+          "$mod SHIFT, J, movewindow, d"
 
-        "$mod CTRL, left, changegroupactive, b"
-        "$mod CTRL, right, changegroupactive, f"
-        "$mod SHIFT, T, lockgroups, toggle"
-        "$mod CTRL SHIFT, left, movewindoworgroup, l"
-        "$mod CTRL SHIFT, right, movewindoworgroup, r"
-        "$mod CTRL SHIFT, up, movewindoworgroup, u"
-        "$mod CTRL SHIFT, down, movewindoworgroup, d"
+          "$mod CTRL, left, changegroupactive, b"
+          "$mod CTRL, right, changegroupactive, f"
+          "$mod SHIFT, T, lockgroups, toggle"
+          "$mod CTRL SHIFT, left, movewindoworgroup, l"
+          "$mod CTRL SHIFT, right, movewindoworgroup, r"
+          "$mod CTRL SHIFT, up, movewindoworgroup, u"
+          "$mod CTRL SHIFT, down, movewindoworgroup, d"
 
-        "$mod, TAB, workspace, previous"
-        "$mod SHIFT, TAB, movetoworkspace, previous"
+          "$mod, TAB, workspace, previous"
+          "$mod SHIFT, TAB, movetoworkspace, previous"
 
-        "$mod, 1, workspace, 1"
-        "$mod, 2, workspace, 2"
-        "$mod, 3, workspace, 3"
-        "$mod, 4, workspace, 4"
-        "$mod, 5, workspace, 5"
-        "$mod, 6, workspace, 6"
-        "$mod, 7, workspace, 7"
-        "$mod, 8, workspace, 8"
-        "$mod, 9, workspace, 9"
-        "$mod, 0, workspace, 10"
-
-        "$mod SHIFT, 1, movetoworkspace, 1"
-        "$mod SHIFT, 2, movetoworkspace, 2"
-        "$mod SHIFT, 3, movetoworkspace, 3"
-        "$mod SHIFT, 4, movetoworkspace, 4"
-        "$mod SHIFT, 5, movetoworkspace, 5"
-        "$mod SHIFT, 6, movetoworkspace, 6"
-        "$mod SHIFT, 7, movetoworkspace, 7"
-        "$mod SHIFT, 8, movetoworkspace, 8"
-        "$mod SHIFT, 9, movetoworkspace, 9"
-        "$mod SHIFT, 0, movetoworkspace, 10"
-
-        "$mod, mouse_down, workspace, e-1"
-        "$mod, mouse_up, workspace, e+1"
-      ];
+          "$mod, mouse_down, workspace, e-1"
+          "$mod, mouse_up, workspace, e+1"
+        ]
+        ++ builtins.concatLists (builtins.genList (n: let
+            key =
+              if n == 9
+              then "0"
+              else builtins.toString (n + 1);
+            workspace = builtins.toString (n + 1);
+          in [
+            "$mod, ${key}, workspace, ${workspace}"
+            "$mod SHIFT, ${key}, movetoworkspace, ${workspace}"
+          ])
+          10);
       bindm = [
         "$mod, mouse:272, movewindow"
         "$mod, mouse:273, resizewindow"
