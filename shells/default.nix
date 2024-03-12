6af5c092e99b2f8dfbe3f99e8a75a433dad526f1
ErrorNoInternet <errornointernet@envs.net>
2024-03-13 01:58:44 +0800

    feat(devShell): add disko

--- shells/default.nix
+++ shells/default.nix
@@ -18,6 +18,7 @@
         delta
         git
         inputs'.agenix.packages.default
+        inputs.disko.packages.default
         self'.packages.neovim
         self'.packages.nix
         statix
