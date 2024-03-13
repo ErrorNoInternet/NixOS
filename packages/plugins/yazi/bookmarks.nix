af2b92494ddfe582f8a4a063e4df9051e649a880
ErrorNoInternet <errornointernet@envs.net>
2024-03-13 15:25:46 +0800

    fix(packages): update yazi plugin hashes
    Accidentally pasted the wrong ones from my clipboard.

--- packages/plugins/yazi/bookmarks.nix
+++ packages/plugins/yazi/bookmarks.nix
@@ -3,5 +3,5 @@ fetchFromGitHub {
   owner = "dedukun";
   repo = "bookmarks.yazi";
   rev = "eebc8644b02e2a5ca95912b36e49c6eada67efca";
-  hash = "sha256-z84cr16uWOHlWEP8KNsY+0VT3LoWOR/KYmUliaRqBJY=";
+  hash = "sha256-QzxNwe0Xeq0eE0X1NLd6U2afGdb3k733hYhUq5K00uo=";
 }
--- packages/plugins/yazi/keyjump.nix
+++ packages/plugins/yazi/keyjump.nix
@@ -3,5 +3,5 @@ fetchFromGitHub {
   owner = "DreamMaoMao";
   repo = "keyjump.yazi";
   rev = "ac018c9a47f003c7f3e9e8b2358bc3c33a2e7050";
-  hash = "sha256-TAFUwHLaA/zsTBiR2qqwcv8NCIaHWnBm7LsnI1fo/o4=";
+  hash = "sha256-z84cr16uWOHlWEP8KNsY+0VT3LoWOR/KYmUliaRqBJY=";
 }
