76c357fb1b80529221d551b49c122ad612b3c21f
ErrorNoInternet <errornointernet@envs.net>
2024-03-14 18:03:16 +0800

    refactor(secrets): more fine-grained inclusion

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -7,11 +7,11 @@ let
   users = {
     NixBtw = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIEi1hNox3FVQ43qxMBNFTUw0guBctlp5DWsD3bD89J2";
   };
-  all = (builtins.attrValues systems) ++ (builtins.attrValues users);
+  allSystems = builtins.attrValues systems;
 in {
   "attic_server-token.age".publicKeys = [users.NixBtw systems.Pix];
   "ddns.age".publicKeys = [users.NixBtw systems.Pix];
   "nheko_access-token.age".publicKeys = [users.NixBtw];
-  "wireless-networks.age".publicKeys = all;
+  "wireless-networks.age".publicKeys = allSystems;
   "workstation-zed.age".publicKeys = [systems.NixBtw];
 }
