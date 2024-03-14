e742c4a5413e313cb1a26c08981c3ada55be8dd1
ErrorNoInternet <errornointernet@envs.net>
2024-03-14 17:58:17 +0800

    refactor(workstation/zed): tweak interval

--- workstation/modules/zfs.nix
+++ workstation/modules/zfs.nix
@@ -28,13 +28,16 @@ in {
       '';
     };
 
+    services.zfs.zed.settings = {
+      ZED_NOTIFY_INTERVAL_SECS = 60;
+      ZED_NOTIFY_VERBOSE = 1;
+    };
+
     environment = {
       systemPackages = [pkgs.ioztat];
 
       etc."zfs/zed.d/zed.rc".text = ''
         source ${config.age.secrets.workstation-zed.path}
-
-        ZED_NOTIFY_VERBOSE=1
       '';
     };
   };
