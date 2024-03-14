c9e6a38024bfac6a1932af5fb4a7d679e0d0dd94
ErrorNoInternet <errornointernet@envs.net>
2024-03-14 14:44:50 +0800

    fix(packages/foot): use latest commit

--- packages/tools/terminals/foot/default.nix
+++ packages/tools/terminals/foot/default.nix
@@ -1,7 +1,17 @@
-{foot}:
-foot.overrideAttrs (oldAttrs: {
+{
+  fetchFromGitea,
+  foot,
+}:
+foot.overrideAttrs (old: {
+  src = fetchFromGitea {
+    domain = "codeberg.org";
+    owner = "dnkl";
+    repo = "foot";
+    rev = "3e6f0e63f3027f19a83c6a2ddcf52018d30f2751";
+    hash = "sha256-hJIA8F5sQdviIEQs72MQaYomlV0pazkPdNsUReADYTo=";
+  };
   patches =
-    (oldAttrs.patches or [])
+    (old.patches or [])
     ++ [
       ./fix-font-misalignment.patch
       ./fullscreen-transparency.patch
