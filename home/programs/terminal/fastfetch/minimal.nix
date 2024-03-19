4f4fce5f302452d146e2e21d889802757ca0ca5a
ErrorNoInternet <errornointernet@envs.net>
2024-03-19 22:09:30 +0800

    fix(fastfetch): use /nix/store for disk

--- home/programs/terminal/fastfetch/minimal.nix
+++ home/programs/terminal/fastfetch/minimal.nix
@@ -41,7 +41,7 @@ builtins.toJSON {
       type = "disk";
       key = "󱥎 storage ";
       format = "{1} / {2} ({3})";
-      folders = "/nix";
+      folders = "/nix/store";
     }
     {
       type = "custom";
