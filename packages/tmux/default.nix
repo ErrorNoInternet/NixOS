54dd11c844934200530a9a75ca8ef772c493598c
ErrorNoInternet <errornointernet@envs.net>
2024-03-20 11:38:50 +0800

    fix(packages/tmux): only optimize for haswell on x86

--- packages/tmux/default.nix
+++ packages/tmux/default.nix
@@ -1,18 +1,20 @@
 {
   fetchFromGitHub,
   lib,
+  system,
   tmux,
   ...
 }:
-lib.overrideDerivation (tmux.overrideAttrs (_: {
-  src = fetchFromGitHub {
-    owner = "tmux";
-    repo = "tmux";
-    rev = "b79e28b2c30e7ef9b1f7ec6233eeb70a1a177231";
-    hash = "sha256-O9a5qL10VCPNSbXxGaRYGq9VZbEsAL/P9ozsvnqhrKM=";
-  };
-
-  patches = [];
-})) (_: {
-  NIX_CFLAGS_COMPILE = "-march=haswell -mtune=haswell";
-})
+tmux.overrideAttrs (old:
+    {
+      src = fetchFromGitHub {
+        owner = "tmux";
+        repo = "tmux";
+        rev = "b79e28b2c30e7ef9b1f7ec6233eeb70a1a177231";
+        hash = "sha256-O9a5qL10VCPNSbXxGaRYGq9VZbEsAL/P9ozsvnqhrKM=";
+      };
+      patches = [];
+    }
+    // lib.attrsets.optionalAttrs (system == "x86_64-linux") {
+      NIX_CFLAGS_COMPILE = (old.NIX_CFLAGS_COMPILE or "") + " -march=haswell -mtune=haswell";
+    })
