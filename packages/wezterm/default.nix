c9f64a26f3df4168c6a35a5cf2c6b9da2a834264
ErrorNoInternet <errornointernet@envs.net>
2024-03-21 12:48:36 +0800

    feat(packages/wezterm): completely remove first configure event

--- packages/wezterm/default.nix
+++ packages/wezterm/default.nix
@@ -9,6 +9,7 @@ wezterm.overrideAttrs (old: {
     (old.patches or [])
     ++ [
       ./optimize-build.patch
+      ./remove-first-configure.patch
       ./remove-set-cursor.patch
     ]
     ++ lib.optional (system == "x86_64-linux") ./optimize-x86-64-build.patch;
