dec1b57f515598f80a921fcf3c71156e501d24e0
ErrorNoInternet <errornointernet@envs.net>
2024-03-21 08:52:50 +0800

    feat(hyprland): bring back size predictions
    I'll be fixing/patching WezTerm to fix this now.

--- packages/hyprland/default.nix
+++ packages/hyprland/default.nix
@@ -3,8 +3,4 @@ inputs'.hyprland.packages.default.overrideAttrs (old: {
   prePatch = ''
     git apply ${./remove-wallpapers.patch}
   '';
-
-  patches =
-    (old.patches or [])
-    ++ [./remove-size-predictions.patch];
 })
