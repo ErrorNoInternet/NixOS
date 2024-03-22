a1f08f89804e4438ba33a7bcb9e3c300339531e6
ErrorNoInternet <errornointernet@envs.net>
2024-03-22 13:03:46 +0800

    refactor(droid): remove extra cache

--- droid/caches/default.nix
+++ droid/caches/default.nix
@@ -1,10 +1,5 @@
-{lib, ...}: {
+{
   imports = [
     ./errornobinaries.nix
   ];
-
-  nix = {
-    substituters = lib.mkOrder 2000 ["https://nix-on-droid.cachix.org"];
-    trustedPublicKeys = ["nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="];
-  };
 }
