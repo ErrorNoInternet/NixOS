77d6cf0c3f8453fce73436e7403f6a83813de4cb
ErrorNoInternet <errornointernet@envs.net>
2024-03-24 18:55:35 +0800

    refactor(home/ssh): increase droid ssh-agent timeout

--- home/programs/terminal/ssh.nix
+++ home/programs/terminal/ssh.nix
@@ -7,17 +7,14 @@
   inherit (lib) mkEnableOption mkIf;
 in {
   options.customPrograms.terminal.ssh.enable =
-    mkEnableOption ""
-    // {
-      default = true;
-    };
+    mkEnableOption "" // {default = true;};
 
   config = mkIf cfg.enable {
     programs.ssh = {
       enable = true;
       addKeysToAgent =
         if config.flags.nixOnDroid
-        then "1h"
+        then "6h"
         else "1d";
     };
     services.ssh-agent.enable = true;
