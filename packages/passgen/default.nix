a9563eef5471b9c1ac364ae2c1884a96d3e9477e
ErrorNoInternet <errornointernet@envs.net>
2024-03-27 19:27:04 +0800

    chore(packages/passgen): update

--- packages/passgen/default.nix
+++ packages/passgen/default.nix
@@ -7,12 +7,15 @@ rustPlatform.buildRustPackage {
   pname = "passgen";
   version = "0.1.0";
 
-  patches = [./reproducible.patch];
-  src = fetchFromGitHub {
-    owner = "ErrorNoInternet";
-    repo = "passgen";
-    rev = "e222f7604edd06a2bc6e366ed91c37c1297173ff";
-    hash = "sha256-Uy0fZUcv3bbM5lack+NMTui9qjFPzCQ+AKnD849lPlU=";
-  };
+  src = let
+    repo = fetchFromGitHub {
+      owner = "ErrorNoInternet";
+      repo = "passgen";
+      rev = "ef1e8b819b22879f5e3350b6fd31637bb7f5a4ec";
+      hash = "sha256-JD4/idZNyFWVUvxBM8WBh6Ori/K4ARW3NtuvuhMIAeo=";
+    };
+  in "${repo}/rust";
   cargoHash = "sha256-/z7QD76wtFiReGmhmhPp3K4E88kFxzkHtXvof5phTEY=";
+
+  patches = [./reproducible.patch];
 }
