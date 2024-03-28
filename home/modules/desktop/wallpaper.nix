ba2626ef72234de55295f26665a66519fd80fa5d
ErrorNoInternet <errornointernet@envs.net>
2024-03-28 20:52:02 +0800

    refactor(home/wallpaper): minor cleanup

--- home/modules/desktop/wallpaper.nix
+++ home/modules/desktop/wallpaper.nix
@@ -8,20 +8,20 @@
   cfg = config.wallpaper;
   inherit (lib) mkEnableOption mkOption mkIf types strings;
 
-  wallpapersPath = "~/pictures/wallpapers";
+  wallpapersPath = "pictures/wallpapers";
 in {
   options.wallpaper = {
     enable = mkEnableOption "" // {default = config.profiles.desktop.enable;};
 
     path = mkOption {
       type = types.str;
-      default = "${wallpapersPath}/${strings.toLower config.colors.schemeName}/0.png";
+      default = "~/${wallpapersPath}/${strings.toLower config.colors.schemeName}/1.png";
     };
   };
 
   config = mkIf cfg.enable {
     home = {
-      file."pictures/wallpapers".source = "${self}/other/wallpapers";
+      file.${wallpapersPath}.source = "${self}/other/wallpapers";
 
       packages = [pkgs.swaybg];
     };
