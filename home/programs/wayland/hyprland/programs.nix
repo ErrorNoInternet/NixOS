47e9c86e56100baf11a4b0f4d0705f4e7da59150
ErrorNoInternet <errornointernet@envs.net>
2024-03-28 20:52:43 +0800

    refactor(hyprland/programs): minor cleanup

--- home/programs/wayland/hyprland/programs.nix
+++ home/programs/wayland/hyprland/programs.nix
@@ -9,11 +9,11 @@ in {
     [
       "hyprctl setcursor ${config.cursor.name} ${builtins.toString config.cursor.size}"
 
+      "waybar"
       "wl-paste --watch cliphist -max-items 1000 store"
-      "sleep 0.25 && waybar"
 
       "swayidle before-sleep 'swaylock --grace 0 --fade-in 0'"
       "swayidle timeout 570 'swaylock --grace 30 --fade-in 30' timeout 1200 'hyprctl dispatch dpms off'"
     ]
-    ++ optional config.wallpaper.enable "swaybg -i ${config.wallpaper.path}";
+    ++ optional config.wallpaper.enable "swaybg -m fill -i ${config.wallpaper.path}";
 }
