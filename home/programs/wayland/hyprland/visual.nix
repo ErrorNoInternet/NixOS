42d85c120f1366bf4f56dda0b4096dbb1e748499
ErrorNoInternet <errornointernet@envs.net>
2024-03-28 12:28:27 +0800

    refactor(hyprland/visual): slide animation for swaync

--- home/programs/wayland/hyprland/visual.nix
+++ home/programs/wayland/hyprland/visual.nix
@@ -65,15 +65,15 @@
     };
 
     extraConfig = ''
-      layerrule=animation fade, hyprpicker
-      layerrule=animation fade, swaync-control-center
-      layerrule=animation fade, wleave
-      layerrule=blur,           rofi
-      layerrule=blur,           swaync-notification-window
-      layerrule=blur,           waybar
-      layerrule=blur,           wleave
-      layerrule=ignorezero,     rofi
-      layerrule=ignorezero,     swaync-notification-window
+      layerrule=animation fade,  hyprpicker
+      layerrule=animation slide, swaync-control-center
+      layerrule=animation fade,  wleave
+      layerrule=blur,            rofi
+      layerrule=blur,            swaync-notification-window
+      layerrule=blur,            waybar
+      layerrule=blur,            wleave
+      layerrule=ignorezero,      rofi
+      layerrule=ignorezero,      swaync-notification-window
     '';
   };
 }
