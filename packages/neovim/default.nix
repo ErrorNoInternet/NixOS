c464bcadbb4968fac60b11e3fbedf3aeeb12edf9
ErrorNoInternet <errornointernet@envs.net>
2024-03-29 12:33:22 +0800

    feat(packages/neovim): optimize

--- packages/neovim/default.nix
+++ packages/neovim/default.nix
@@ -1,6 +1,14 @@
-{inputs', ...}:
-inputs'.neovim-nightly.packages.default.overrideAttrs (old: {
-  patches =
-    (old.patches or [])
-    ++ [./remove-fold-numbers.patch];
-})
+{
+  cpus,
+  inputs',
+  self,
+  system,
+  ...
+}:
+self.lib.derivations.optimizeNative {inherit cpus system;}
+(self.lib.derivations.optimizeLto (inputs'.neovim-nightly.packages.default.overrideAttrs
+  (old: {
+    patches =
+      (old.patches or [])
+      ++ [./remove-fold-numbers.patch];
+  })))
