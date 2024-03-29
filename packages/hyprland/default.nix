6bb9a2ed2e3a08b7bb22a7a5bae2b938e1bcb521
ErrorNoInternet <errornointernet@envs.net>
2024-03-29 20:56:22 +0800

    refactor(packages/hyprland): remove dead code

--- packages/hyprland/default.nix
+++ packages/hyprland/default.nix
@@ -7,8 +7,8 @@
 }:
 with self.lib.derivations;
   optimizeArchitecture {inherit architectures system;}
-  (optimizeLtoMeson (inputs'.hyprland.packages.default.overrideAttrs (old: {
+  (optimizeLtoMeson (inputs'.hyprland.packages.default.overrideAttrs {
     prePatch = ''
       git apply ${./remove-wallpapers.patch}
     '';
-  })))
+  }))
