cd2b46e1dd3bfac3161aaabdd1ef372c98b8b2ef
ErrorNoInternet <errornointernet@envs.net>
2024-03-29 12:23:10 +0800

    fix(hyprland): try to re-enable lto

--- packages/hyprland/default.nix
+++ packages/hyprland/default.nix
@@ -6,8 +6,10 @@
   ...
 }:
 self.lib.derivations.optimizeNative {inherit cpus system;}
-(inputs'.hyprland.packages.default.overrideAttrs {
+(inputs'.hyprland.packages.default.overrideAttrs (old: {
   prePatch = ''
     git apply ${./remove-wallpapers.patch}
   '';
-})
+
+  mesonBuildFlags = (old.mesonBuildFlags or []) ++ ["-Db_lto=true"];
+}))
