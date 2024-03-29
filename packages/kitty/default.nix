e8c056488c7ee904f539b25ebc8f4bb57ac69876
ErrorNoInternet <errornointernet@envs.net>
2024-03-29 19:53:53 +0800

    feat(packages/kitty): optimize (lto + architecture)

--- packages/kitty/default.nix
+++ packages/kitty/default.nix
@@ -1,10 +1,18 @@
-{kitty, ...}:
-kitty.overrideAttrs (old: {
-  patches =
-    (old.patches or [])
-    ++ [
-      ./parse-base10-rgb.patch
-      ./remove-resize-text.patch
-      ./thicker-braille-dots.patch
-    ];
-})
+{
+  architectures,
+  kitty,
+  self,
+  system,
+  ...
+}:
+with self.lib.derivations;
+  optimizeArchitecture {inherit architectures system;}
+  (optimizeLto (kitty.overrideAttrs (old: {
+    patches =
+      (old.patches or [])
+      ++ [
+        ./parse-base10-rgb.patch
+        ./remove-resize-text.patch
+        ./thicker-braille-dots.patch
+      ];
+  })))
