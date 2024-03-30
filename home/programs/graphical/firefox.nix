b28804da4e9cfc5c35ba70939c4fe84f0c90d66d
ErrorNoInternet <errornointernet@envs.net>
2024-03-30 13:15:46 +0800

    refactor(home/firefox): final cleanup

--- home/programs/graphical/firefox.nix
+++ home/programs/graphical/firefox.nix
@@ -29,7 +29,6 @@ in {
         extensions = with config.nur.repos.rycee.firefox-addons; [
           behind-the-overlay-revival
           darkreader
-          kristofferhagen-nord-theme
           playback-speed
           privacy-badger
           return-youtube-dislikes
@@ -66,7 +65,6 @@ in {
           "datareporting.healthreport.uploadEnabled" = false;
           "datareporting.policy.dataSubmissionEnabled" = false;
           "devtools.onboarding.telemetry.logged" = false;
-          "extensions.activeThemeID" = "{e410fec2-1cbd-4098-9944-e21e708418af}";
           "extensions.formautofill.addresses.enabled" = false;
           "extensions.formautofill.available" = "off";
           "extensions.formautofill.creditCards.available" = false;
@@ -99,14 +97,6 @@ in {
           "toolkit.telemetry.unified" = false;
           "toolkit.telemetry.updatePing.enabled" = false;
           "widget.use-xdg-desktop-portal.file-picker" = 1;
-
-          "extensions.webextensions.ExtensionStorageIDB.migrated.addon@darkreader.org" = true;
-          "extensions.webextensions.ExtensionStorageIDB.migrated.jid1-MnnxcxisBPnSXQ@jetpack" = true;
-          "extensions.webextensions.ExtensionStorageIDB.migrated.screenshots@mozilla.org" = true;
-          "extensions.webextensions.ExtensionStorageIDB.migrated.uBlock0@raymondhill.net" = true;
-          "extensions.webextensions.ExtensionStorageIDB.migrated.{a6c4a591-f1b2-4f03-b3ff-767e5bedf4e7}" = true;
-          "extensions.webextensions.ExtensionStorageIDB.migrated.{c0e1baea-b4cb-4b62-97f0-278392ff8c37}" = true;
-          "extensions.webextensions.ExtensionStorageIDB.migrated.{d7742d87-e61d-4b78-b8a1-b469842139fa}" = true;
         };
 
         search = {
