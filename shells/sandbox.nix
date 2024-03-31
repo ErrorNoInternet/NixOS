5ee692dcd0fa1ccd6c3f75d720f23992555be5c5
ErrorNoInternet <errornointernet@envs.net>
2024-04-01 01:41:35 +0800

    refactor(shells/sandbox): minor cleanup

--- shells/sandbox.nix
+++ shells/sandbox.nix
@@ -3,30 +3,32 @@
     devShells.sandbox = pkgs.mkShell {
       name = "sandbox";
 
-      shellHook = "exec bash";
-
       packages = with pkgs; let
         mkNixPak = inputs.nixpak.lib.nixpak {inherit lib pkgs;};
       in [
         (mkNixPak {
           config = _: {
             app.package = bash;
-
             bubblewrap.bind.ro = let
               paths = [
                 "curl"
+                "which"
               ];
             in
               map (path: "/run/current-system/sw/bin/${path}") paths;
 
             etc.sslCertificates.enable = true;
-
             dbus.enable = false;
           };
         })
         .config
         .script
       ];
+
+      shellHook = ''
+        which bash
+        exec bash
+      '';
     };
   };
 }
