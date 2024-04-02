fab6b9aa5ae307259a1cd4971c559691ad2c9c7b
ErrorNoInternet <errornointernet@envs.net>
2024-04-02 14:06:32 +0800

    feat: sandbox spotify

--- home/programs/graphical/spotify.nix
+++ home/programs/graphical/spotify.nix
@@ -3,6 +3,7 @@
   inputs',
   inputs,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.customPrograms.graphical.spotify;
@@ -10,9 +11,7 @@
 
   spicePkgs = inputs'.spicetify-nix.packages.default;
 in {
-  imports = [
-    inputs.spicetify-nix.homeManagerModule
-  ];
+  imports = [inputs.spicetify-nix.homeManagerModule];
 
   options.customPrograms.graphical.spotify.enable =
     mkEnableOption "";
@@ -22,6 +21,8 @@ in {
 
     programs.spicetify = {
       enable = true;
+      dontInstall = true;
+
       enabledExtensions = with spicePkgs.extensions; [
         adblock
         fullAppDisplay
@@ -31,5 +32,70 @@ in {
       theme = spicePkgs.themes.text;
       colorScheme = config.colors.schemeName;
     };
+
+    home.packages = let
+      unwrapped-spotify = config.programs.spicetify.spicedSpotify;
+
+      sandboxed-spotify =
+        (inputs.nixpak.lib.nixpak {inherit lib pkgs;} {
+          config = {sloth, ...}: rec {
+            app.package = unwrapped-spotify;
+
+            etc.sslCertificates.enable = true;
+            flatpak.appId = "com.spotify.Client";
+
+            dbus.policies = {
+              "${flatpak.appId}" = "own";
+              "org.freedesktop.portal.Desktop" = "talk";
+              "org.freedesktop.portal.OpenURI" = "talk";
+              "org.gnome.SettingsDaemon.MediaKeys" = "talk";
+              "org.mpris.MediaPlayer2.spotify" = "own";
+            };
+
+            fonts = {
+              enable = true;
+              fonts = with pkgs; [
+                cantarell-fonts
+                dejavu_fonts
+                gyre-fonts
+                liberation_ttf
+                noto-fonts-cjk-sans
+                noto-fonts-cjk-serif
+                noto-fonts-color-emoji
+                source-code-pro
+                source-sans
+              ];
+            };
+
+            bubblewrap = {
+              bind = {
+                ro = [
+                  "/tmp/.X11-unix"
+                  (sloth.concat' sloth.xdgDataHome "/gtk-3.0")
+                  (sloth.concat' sloth.xdgDataHome "/icons")
+                ];
+                rw = [
+                  (sloth.concat' sloth.xdgCacheHome "/spotify")
+                  (sloth.concat' sloth.xdgConfigHome "/spotify")
+                ];
+              };
+
+              sockets = {
+                pulse = true;
+                wayland = true;
+              };
+            };
+          };
+        })
+        .config
+        .script;
+    in [
+      (pkgs.runCommand "spotify-metadata" {} ''
+        mkdir -p $out
+        cp -a ${unwrapped-spotify}/share $out
+      '')
+
+      sandboxed-spotify
+    ];
   };
 }
