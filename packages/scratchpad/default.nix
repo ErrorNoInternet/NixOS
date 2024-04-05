dde2e47a7c94c8872ad37eb451a3e0ca311282c6
ErrorNoInternet <errornointernet@envs.net>
2024-04-05 22:02:56 +0800

    feat(packages/scratchpad): hide rofi icons

--- packages/scratchpad/default.nix
+++ packages/scratchpad/default.nix
@@ -4,6 +4,7 @@ inputs'.hyprwm-contrib.packages.scratchpad.overrideAttrs (old: {
     (old.patches or [])
     ++ [
       ./fix-grammar.patch
+      ./hide-rofi-icons.patch
       ./transient-notifications.patch
     ];
 })
