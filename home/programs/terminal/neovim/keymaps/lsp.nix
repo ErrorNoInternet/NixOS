890a738b62dad34a7285bd4f7c9611755e2e6d89
ErrorNoInternet <errornointernet@envs.net>
2024-04-08 14:37:43 +0800

    fix(neovim/keymaps/lsp): use mkRaw

--- home/programs/terminal/neovim/keymaps/lsp.nix
+++ home/programs/terminal/neovim/keymaps/lsp.nix
@@ -1,14 +1,16 @@
 {lib, ...}: {
-  programs.nixvim = {
+  programs.nixvim = {helpers, ...}: let
+    inherit (helpers) mkRaw;
+  in {
     plugins.cmp = let
       mkPreset = type: ''
         cmp.mapping.preset.${type}(${lib.generators.toLua {} {
-          "<C-down>" = "cmp.mapping.scroll_docs(4)";
-          "<C-e>" = "cmp.mapping.abort()";
-          "<C-up>" = "cmp.mapping.scroll_docs(-4)";
-          "<CR>" = "cmp.mapping.confirm()";
-          "<S-Tab>" = "cmp.mapping.select_prev_item()";
-          "<Tab>" = "cmp.mapping.select_next_item()";
+          "<C-down>" = mkRaw "cmp.mapping.scroll_docs(4)";
+          "<C-e>" = mkRaw "cmp.mapping.abort()";
+          "<C-up>" = mkRaw "cmp.mapping.scroll_docs(-4)";
+          "<CR>" = mkRaw "cmp.mapping.confirm()";
+          "<S-Tab>" = mkRaw "cmp.mapping.select_prev_item()";
+          "<Tab>" = mkRaw "cmp.mapping.select_next_item()";
         }})
       '';
     in {
