2b63dbeb18681246da5284b7f4c60308b304d508
ErrorNoInternet <errornointernet@envs.net>
2024-04-09 18:01:46 +0800

    feat(packages/nix): fix eval cache patch
    Taken from PR 10368.

--- packages/nix/default.nix
+++ packages/nix/default.nix
@@ -5,7 +5,9 @@
   ...
 }:
 self.lib.derivations.c.optimizeAll host
-(inputs'.nix-super.packages.default.overrideAttrs {
+(inputs'.nix-super.packages.default.overrideAttrs (old: {
+  patches = (old.patches or []) ++ [./fix-eval-cache.patch];
+
   doCheck = false;
   doInstallCheck = false;
-})
+}))
