68b0af4e7363fc9349cf1653c98a8d49f05b8a36
ErrorNoInternet <errornointernet@envs.net>
2024-04-10 16:56:40 +0800

    fix(shared/system): move packages module into nixos

--- nixos/modules/default.nix
+++ nixos/modules/default.nix
@@ -4,6 +4,7 @@
     ./emulation.nix
     ./flags.nix
     ./host.nix
+    ./packages.nix
     ./raspberry-pi.nix
     ./username.nix
     ./wireless.nix
--- shared/system/modules/default.nix
+++ shared/system/modules/default.nix
@@ -1,6 +1,5 @@
 {
   imports = [
     ./host.nix
-    ./packages.nix
   ];
 }
