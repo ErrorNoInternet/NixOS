81e7c072e5736a6071bb208dcaa249d5725952dd
ErrorNoInternet <errornointernet@envs.net>
2024-04-11 16:17:03 +0800

    refactor(fastfetch): clean up

--- home/programs/terminal/fastfetch/default.nix
+++ home/programs/terminal/fastfetch/default.nix
@@ -3,7 +3,7 @@
   lib,
   pkgs,
   ...
-}: let
+} @ args: let
   cfg = config.customPrograms.fastfetch;
   inherit (lib) mkEnableOption mkIf;
 in {
@@ -11,47 +11,39 @@ in {
     mkEnableOption "" // {default = config.customPrograms.fish.enable;};
 
   config = mkIf cfg.enable {
-    home = {
-      packages = with pkgs;
-        map (variation: (writeScriptBin variation ''
-          export PATH=$PATH:${nix}/bin
-          ${fastfetch}/bin/${variation} "$@"
-        ''))
-        ["fastfetch" "flashfetch"];
+    home.packages = [pkgs.fastfetch];
 
-      file = let
-        escape = string: builtins.replaceStrings ["\\\\"] ["\\"] string;
-      in {
-        "${config.xdg.configHome}/fastfetch/minimal.jsonc".text = escape (import ./minimal.nix {inherit pkgs;});
-        "${config.xdg.configHome}/fastfetch/minimal-droid.jsonc".text = escape (import ./minimal-droid.nix {inherit pkgs;});
-
-        "${config.xdg.configHome}/fastfetch/config.jsonc".text = builtins.toJSON {
-          general.processingTimeout = 10000;
-          modules = [
-            "title"
-            "separator"
-            "os"
-            "kernel"
-            "uptime"
-            "packages"
-            "shell"
-            "display"
-            "wm"
-            "theme"
-            "icons"
-            "cursor"
-            "terminalfont"
-            "terminal"
-            "processes"
-            "memory"
-            {
-              type = "disk";
-              folders = "/";
-            }
-            "break"
-            "colors"
-          ];
-        };
+    xdg.configFile = let
+      escape = string: builtins.replaceStrings ["\\\\"] ["\\"] string;
+    in {
+      "fastfetch/minimal.jsonc".text = escape (import ./minimal.nix args);
+      "fastfetch/minimal-droid.jsonc".text = escape (import ./minimal-droid.nix args);
+      "fastfetch/config.jsonc".text = builtins.toJSON {
+        general.processingTimeout = 10000;
+        modules = [
+          "title"
+          "separator"
+          "os"
+          "kernel"
+          "uptime"
+          "packages"
+          "shell"
+          "display"
+          "wm"
+          "theme"
+          "icons"
+          "cursor"
+          "terminalfont"
+          "terminal"
+          "processes"
+          "memory"
+          {
+            type = "disk";
+            folders = "/";
+          }
+          "break"
+          "colors"
+        ];
       };
     };
   };
