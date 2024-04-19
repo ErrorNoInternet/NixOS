862ae42b4960d4aadc8bee223a1e47f713fcbba2
ErrorNoInternet <errornointernet@envs.net>
2024-04-20 00:32:42 +0800

    refactor(packages/eza): clean up patch

--- packages/eza/default.nix
+++ packages/eza/default.nix
@@ -5,5 +5,5 @@
   ...
 }:
 self.lib.derivations.rust.optimizeArchitecture host (eza.overrideAttrs (old: {
-  patches = (old.patches or []) ++ [./zfs-directory-size.patch];
+  patches = (old.patches or []) ++ [./zfs-optimizations.patch];
 }))
