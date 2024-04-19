fa95a22cbfbc94caadbe1c112a8cd19297b684af
ErrorNoInternet <errornointernet@envs.net>
2024-04-19 10:23:39 +0800

    feat(packages/zfs-unstable): add zap shrinking patch

--- packages/zfs-unstable/source.nix
+++ packages/zfs-unstable/source.nix
@@ -15,8 +15,14 @@
     ./fiemap.patch
 
     (fetchpatch {
+      name = "zap-shrinking.patch";
+      url = "https://github.com/openzfs/zfs/pull/15888.diff";
+      hash = "sha256-Hx4jOqDQo2wTTRVWX48dnBvFE8uQIP1yL6JQ3Akgrjo=";
+    })
+
+    (fetchpatch {
       name = "chacha20-poly1305.patch";
-      url = "https://github.com/openzfs/zfs/pull/14249.patch";
+      url = "https://github.com/openzfs/zfs/pull/14249.diff";
       hash = "sha256-T2gO82f20zkAnmrx8UGwKf307y05gfgDENPif9PdbLs=";
     })
   ];
