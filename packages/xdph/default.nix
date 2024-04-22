bf8b1a0d4c768acb84e580cef5094c813a5ac667
ErrorNoInternet <errornointernet@envs.net>
2024-04-22 16:08:24 +0800

    fix(packages/xdph): use custom slurp

--- packages/xdph/default.nix
+++ packages/xdph/default.nix
@@ -1,4 +1,13 @@
-{xdg-desktop-portal-hyprland, ...}:
-xdg-desktop-portal-hyprland.overrideAttrs (old: {
+{
+  callPackage,
+  host,
+  self,
+  xdg-desktop-portal-hyprland,
+  ...
+}:
+(xdg-desktop-portal-hyprland.overrideAttrs (old: {
   patches = (old.patches or []) ++ [./move-checkbox.patch];
-})
+}))
+.override {
+  slurp = callPackage ../slurp {inherit host self;};
+}
