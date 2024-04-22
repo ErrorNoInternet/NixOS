c716269abecd9d97b27ef779bcb885900a180b82
ErrorNoInternet <errornointernet@envs.net>
2024-04-22 15:23:00 +0800

    refactor: clean up xdph

--- flake.nix
+++ flake.nix
@@ -48,10 +48,7 @@
 
     hyprland = {
       url = "github:hyprwm/Hyprland";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        xdph.follows = "xdph";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     hyprwm-contrib = {
@@ -142,16 +139,6 @@
       inputs = {
         flake-utils.follows = "flake-utils";
         nixpkgs.follows = "nixpkgs";
-      };
-    };
-
-    xdph = {
-      url = "github:hyprwm/xdg-desktop-portal-hyprland";
-      inputs = {
-        hyprland-protocols.follows = "hyprland/hyprland-protocols";
-        hyprlang.follows = "hyprland/hyprlang";
-        nixpkgs.follows = "hyprland/nixpkgs";
-        systems.follows = "hyprland/systems";
       };
     };
   };
--- packages/xdph/default.nix
+++ packages/xdph/default.nix
@@ -1,4 +1,4 @@
-{inputs', ...}:
-inputs'.xdph.packages.default.overrideAttrs (old: {
+{xdg-desktop-portal-hyprland, ...}:
+xdg-desktop-portal-hyprland.overrideAttrs (old: {
   patches = (old.patches or []) ++ [./move-checkbox.patch];
 })
