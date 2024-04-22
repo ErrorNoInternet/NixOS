01b153268de9e8ce42cd61bb264de7762fafe2ac
ErrorNoInternet <errornointernet@envs.net>
2024-04-22 16:20:20 +0800

    fix(packages/slurp): set option box color as well

--- packages/slurp/default.nix
+++ packages/slurp/default.nix
@@ -9,6 +9,6 @@ self.lib.derivations.c.optimizeAll host (slurp.overrideAttrs (old: {
   nativeBuildInputs = (old.nativeBuildInputs or []) ++ [makeWrapper];
   postFixup = ''
     wrapProgram $out/bin/slurp \
-      --add-flags "-b \"#eceff420\" -c \"#2e3440ff\""
+      --add-flags "-b \"#eceff420\" -B \"#eceff420\" -c \"#2e3440ff\""
   '';
 }))
