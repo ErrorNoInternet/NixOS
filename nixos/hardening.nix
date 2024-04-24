8db003a77a8d46a62117719fd2248f818bdf22a9
ErrorNoInternet <errornointernet@envs.net>
2024-04-24 22:45:41 +0800

    feat(nixos): more kernel hardening

---
+++ nixos/hardening.nix
@@ -0,0 +1,40 @@
+{
+  boot = {
+    kernel.sysctl = {
+      "net.ipv4.conf.all.accept_redirects" = 0;
+      "net.ipv4.conf.all.secure_redirects" = 0;
+      "net.ipv4.conf.default.accept_redirects" = 0;
+      "net.ipv4.conf.default.secure_redirects" = 0;
+      "net.ipv6.conf.all.accept_redirects" = 0;
+      "net.ipv6.conf.default.accept_redirects" = 0;
+
+      "net.ipv4.tcp_fin_timeout" = 30;
+
+      "vm.page-cluster" = 0;
+      "vm.swappiness" = 180;
+    };
+
+    blacklistedKernelModules = [
+      "ax25"
+      "netrom"
+      "rose"
+
+      "adfs"
+      "affs"
+      "befs"
+      "bfs"
+      "cramfs"
+      "efs"
+      "exofs"
+      "freevxfs"
+      "hfs"
+      "hpfs"
+      "jfs"
+      "minix"
+      "omfs"
+      "qnx4"
+      "qnx6"
+      "sysv"
+    ];
+  };
+}
