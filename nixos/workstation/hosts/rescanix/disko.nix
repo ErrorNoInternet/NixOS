f2ab8d49e036572ea7a8abe852daa24649cad921
ErrorNoInternet <errornointernet@envs.net>
2024-05-06 11:44:56 +0800

    feat(rescanix): switch to btrfs on root

--- nixos/workstation/hosts/rescanix/disko.nix
+++ nixos/workstation/hosts/rescanix/disko.nix
@@ -1,106 +1,50 @@
 {config, ...}: {
-  disko.devices = {
-    disk.primary = {
-      type = "disk";
-      device = "/dev/sda";
-      content = {
-        type = "gpt";
-        partitions = {
-          ESP = {
-            size = "512M";
-            type = "EF00";
-            label = "BOOT";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
-          };
+  disko.devices.disk.primary = {
+    type = "disk";
+    device = "/dev/sda";
 
-          root = {
-            size = "100%";
-            content = {
-              type = "zfs";
-              pool = config.host.name;
-            };
+    content = {
+      type = "gpt";
+
+      partitions = {
+        ESP = {
+          size = "512M";
+          type = "EF00";
+          label = "BOOT";
+          content = {
+            type = "filesystem";
+            format = "vfat";
+            mountpoint = "/boot";
           };
         };
-      };
-    };
 
-    zpool.${config.host.name} = {
-      type = "zpool";
-
-      options = {
-        ashift = "12";
-        compatibility = "openzfs-2.2-linux";
-      };
-
-      rootFsOptions = {
-        acltype = "posix";
-        compression = "zstd";
-        dnodesize = "auto";
-        mountpoint = "none";
-        normalization = "formD";
-        xattr = "sa";
-      };
-
-      datasets = {
         root = {
-          mountpoint = "/";
-          type = "zfs_fs";
-          options = {
-            mountpoint = "legacy";
-            recordsize = "64K";
-          };
-        };
-        "root/home" = {
-          mountpoint = "/home";
-          type = "zfs_fs";
-          options = {
-            mountpoint = "legacy";
-            refreservation = "1G";
-          };
-        };
-        "root/var" = {
-          mountpoint = "/var";
-          type = "zfs_fs";
-          options.mountpoint = "legacy";
-        };
-        "root/var/coredump" = {
-          mountpoint = "/var/lib/systemd/coredump";
-          type = "zfs_fs";
-          options = {
-            atime = "off";
-            compression = "zle";
-            mountpoint = "legacy";
-            recordsize = "1M";
-          };
-        };
-        "root/var/log" = {
-          mountpoint = "/var/log";
-          type = "zfs_fs";
-          options = {
-            atime = "off";
-            mountpoint = "legacy";
-            recordsize = "16K";
-          };
-        };
-        "root/nix" = {
-          mountpoint = "/nix";
-          type = "zfs_fs";
-          options = {
-            atime = "off";
-            mountpoint = "legacy";
-            recordsize = "32K";
-          };
-        };
-        "root/nix/store" = {
-          mountpoint = "/nix/store";
-          type = "zfs_fs";
-          options = {
-            mountpoint = "legacy";
-            recordsize = "256K";
+          size = "100%";
+          content = {
+            type = "btrfs";
+            extraArgs = [
+              "-L ${config.host.name}"
+              "-O bgt"
+            ];
+
+            subvolumes = {
+              "@" = {
+                mountpoint = "/";
+                mountOptions = ["compress=zstd"];
+              };
+              "@home" = {
+                mountpoint = "/home";
+                mountOptions = ["compress=zstd"];
+              };
+              "@var" = {
+                mountpoint = "/home";
+                mountOptions = ["compress=zstd"];
+              };
+              "@nix" = {
+                mountpoint = "/nix";
+                mountOptions = ["compress=zstd" "noatime"];
+              };
+            };
           };
         };
       };
