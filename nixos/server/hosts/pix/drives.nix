579b38656eb01cb9d5c01df95595c5e8e2bedba6
ErrorNoInternet <errornointernet@envs.net>
2024-05-07 11:00:48 +0800

    refactor(pix/drives): remove subvol for drive3

--- nixos/server/hosts/pix/drives.nix
+++ nixos/server/hosts/pix/drives.nix
@@ -10,7 +10,7 @@
   fileSystems."/mnt/drive3" = {
     device = "/dev/disk/by-uuid/6a03c0f9-5c76-4a08-9091-aba7239a6429";
     fsType = "btrfs";
-    options = ["subvol=root" "relatime" "x-systemd.automount" "noauto"];
+    options = ["relatime" "x-systemd.automount" "noauto"];
   };
 
   services = {
