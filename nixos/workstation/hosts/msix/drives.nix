14e582b5d232b9f9393f7d7f7ce26878866b8ccf
ErrorNoInternet <errornointernet@envs.net>
2024-05-15 17:56:55 +0800

    feat(msix/drives): snapshot data root

--- nixos/workstation/hosts/msix/drives.nix
+++ nixos/workstation/hosts/msix/drives.nix
@@ -16,6 +16,18 @@
         TIMELINE_LIMIT_YEARLY = 0;
       };
 
+      data = {
+        SUBVOLUME = "/mnt/data";
+        TIMELINE_CREATE = true;
+        TIMELINE_CLEANUP = true;
+
+        TIMELINE_LIMIT_HOURLY = 24;
+        TIMELINE_LIMIT_DAILY = 7;
+        TIMELINE_LIMIT_WEEKLY = 0;
+        TIMELINE_LIMIT_MONTHLY = 0;
+        TIMELINE_LIMIT_YEARLY = 0;
+      };
+
       backups = {
         SUBVOLUME = "/mnt/data/backups";
         TIMELINE_CREATE = true;
