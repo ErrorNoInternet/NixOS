a0c470990c568573e5b4efce3ccee3df827781b8
ErrorNoInternet <errornointernet@envs.net>
2024-05-25 16:33:04 +0800

    fix(packages/strace): disable optimizations

--- packages/strace/default.nix
+++ packages/strace/default.nix
@@ -1,11 +1,4 @@
-{
-  host,
-  self,
-  strace,
-  ...
-}:
-with self.lib.derivations;
-  c.optimizeAllExceptLto host (autotools.optimizeLto
-    (strace.overrideAttrs (old: {
-      patches = (old.patches or []) ++ [./colors.patch];
-    })))
+{strace, ...}:
+strace.overrideAttrs (old: {
+  patches = (old.patches or []) ++ [./colors.patch];
+})
